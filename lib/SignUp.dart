36accc92886d709502c378e16eca055c113ef413
Lucas Sousa <seu@email.com>
2024-02-21 17:42:52 +0100

    [ADD] Addying gender, and birthday in SignUp

--- lib/SignUp.dart
+++ lib/SignUp.dart
@@ -1,56 +1,173 @@
 import 'package:flutter/material.dart';
 import 'package:churchapp/HomePage.dart';
 
-class SignUp extends StatelessWidget {
+class SignUp extends StatefulWidget {
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  String gender = ''; // Variável para armazenar o gênero selecionado
+  int day = 1; // Variáveis para armazenar dia, mês e ano da idade
+  String month = 'January';
+  int year = 2000;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Registration'),
+        title: Text('SignUp'),
       ),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'Full Name',
-                ),
-              ),
-              SizedBox(height: 20.0),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                ),
-              ),
-              SizedBox(height: 20.0),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                ),
-              ),
-              SizedBox(height: 20.0),
-              ElevatedButton(
-                onPressed: () {
-                  // Navigate para a tela de SignUp
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => HomePage()),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  primary: Color.fromARGB(255, 90, 175, 249),
-                  onPrimary: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20.0),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Full Name',
                   ),
                 ),
-                child: Text('Sign Up'),
-              ),
-            ],
+                SizedBox(height: 20.0),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                Row(
+                  children: [
+                    Expanded(
+                      child: DropdownButtonFormField<String>(
+                        value: gender,
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            gender = newValue!;
+                          });
+                        },
+                        items: <String>['', 'Male', 'Female']
+                            .map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        decoration: InputDecoration(
+                          labelText: 'Gender',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20.0),
+                Row(
+                  children: [
+                    Expanded(
+                      child: DropdownButtonFormField<int>(
+                        value: day,
+                        onChanged: (int? newValue) {
+                          setState(() {
+                            day = newValue!;
+                          });
+                        },
+                        items: List.generate(31, (index) => index + 1)
+                            .map<DropdownMenuItem<int>>((int value) {
+                          return DropdownMenuItem<int>(
+                            value: value,
+                            child: Text(value.toString()),
+                          );
+                        }).toList(),
+                        decoration: InputDecoration(
+                          labelText: 'Day',
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 20.0),
+                    Expanded(
+                      child: DropdownButtonFormField<String>(
+                        value: month,
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            month = newValue!;
+                          });
+                        },
+                        items: <String>[
+                          'January',
+                          'February',
+                          'March',
+                          'April',
+                          'May',
+                          'June',
+                          'July',
+                          'August',
+                          'September',
+                          'October',
+                          'November',
+                          'December'
+                        ].map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        decoration: InputDecoration(
+                          labelText: 'Month',
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 20.0),
+                    Expanded(
+                      child: DropdownButtonFormField<int>(
+                        value: year,
+                        onChanged: (int? newValue) {
+                          setState(() {
+                            year = newValue!;
+                          });
+                        },
+                        items: List.generate(82, (index) => 1940 + index)
+                            .map<DropdownMenuItem<int>>((int value) {
+                          return DropdownMenuItem<int>(
+                            value: value,
+                            child: Text(value.toString()),
+                          );
+                        }).toList(),
+                        decoration: InputDecoration(
+                          labelText: 'Year',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () {
+                    // Navigate para a tela de SignUp
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => HomePage()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Color.fromARGB(255, 90, 175, 249),
+                    onPrimary: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                  child: Text('Sign Up'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
