d68f4a9bd26cddb7b639e732a202f611d872a452
Lucas Sousa <seu@email.com>
2024-02-22 14:41:33 +0100

    [ADD] Adding SignUp Steps

--- lib/SignUp/SignUp.dart
+++ lib/SignUp/SignUp.dart
@@ -7,11 +7,6 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
-  String gender = ''; // Variável para armazenar o gênero selecionado
-  int day = 1; // Variáveis para armazenar dia, mês e ano da idade
-  String month = 'January';
-  int year = 2000;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,127 +21,17 @@ class _SignUpState extends State<SignUp> {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
+                SizedBox(height: 200.0),
                 TextField(
                   decoration: InputDecoration(
-                    labelText: 'Full Name',
+                    labelText: 'First Name',
                   ),
                 ),
                 SizedBox(height: 20.0),
                 TextField(
                   decoration: InputDecoration(
-                    labelText: 'Email',
+                    labelText: 'Last Name',
                   ),
-                ),
-                SizedBox(height: 20.0),
-                TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                  ),
-                ),
-                SizedBox(height: 20.0),
-                Row(
-                  children: [
-                    Expanded(
-                      child: DropdownButtonFormField<String>(
-                        value: gender,
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            gender = newValue!;
-                          });
-                        },
-                        items: <String>['', 'Male', 'Female']
-                            .map<DropdownMenuItem<String>>((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                        decoration: InputDecoration(
-                          labelText: 'Gender',
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 20.0),
-                Row(
-                  children: [
-                    Expanded(
-                      child: DropdownButtonFormField<int>(
-                        value: day,
-                        onChanged: (int? newValue) {
-                          setState(() {
-                            day = newValue!;
-                          });
-                        },
-                        items: List.generate(31, (index) => index + 1)
-                            .map<DropdownMenuItem<int>>((int value) {
-                          return DropdownMenuItem<int>(
-                            value: value,
-                            child: Text(value.toString()),
-                          );
-                        }).toList(),
-                        decoration: InputDecoration(
-                          labelText: 'Day',
-                        ),
-                      ),
-                    ),
-                    SizedBox(width: 20.0),
-                    Expanded(
-                      child: DropdownButtonFormField<String>(
-                        value: month,
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            month = newValue!;
-                          });
-                        },
-                        items: <String>[
-                          'January',
-                          'February',
-                          'March',
-                          'April',
-                          'May',
-                          'June',
-                          'July',
-                          'August',
-                          'September',
-                          'October',
-                          'November',
-                          'December'
-                        ].map<DropdownMenuItem<String>>((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                        decoration: InputDecoration(
-                          labelText: 'Month',
-                        ),
-                      ),
-                    ),
-                    SizedBox(width: 20.0),
-                    Expanded(
-                      child: DropdownButtonFormField<int>(
-                        value: year,
-                        onChanged: (int? newValue) {
-                          setState(() {
-                            year = newValue!;
-                          });
-                        },
-                        items: List.generate(82, (index) => 1940 + index)
-                            .map<DropdownMenuItem<int>>((int value) {
-                          return DropdownMenuItem<int>(
-                            value: value,
-                            child: Text(value.toString()),
-                          );
-                        }).toList(),
-                        decoration: InputDecoration(
-                          labelText: 'Year',
-                        ),
-                      ),
-                    ),
-                  ],
                 ),
                 SizedBox(height: 20.0),
                 ElevatedButton(
@@ -164,7 +49,7 @@ class _SignUpState extends State<SignUp> {
                       borderRadius: BorderRadius.circular(20.0),
                     ),
                   ),
-                  child: Text('Sign Up'),
+                  child: Text('Next'),
                 ),
               ],
             ),
