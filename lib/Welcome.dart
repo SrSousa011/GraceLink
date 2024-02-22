c109221825c9353751d0967251882788b19de856
Lucas Sousa <seu@email.com>
2024-02-22 12:24:52 +0100

    [ADD] Wlecome adding home image

--- lib/Welcome.dart
+++ lib/Welcome.dart
@@ -14,9 +14,9 @@ class Welcome extends StatelessWidget {
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage(
-                'assets/background_image.jpg'), // Caminho relativo da imagem de fundo
-            fit: BoxFit.cover,
+            image: AssetImage('assets/imagens/background_image.jpg'),
+            fit: BoxFit
+                .fill, // Alterado para BoxFit.fill para preencher toda a tela
           ),
         ),
         child: Column(
@@ -25,19 +25,19 @@ class Welcome extends StatelessWidget {
             Spacer(),
             Text(
               'Resplandecendo',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
             ),
             Text(
               'as',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
             ),
             Text(
               'Naçoes - Athus',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
             ),
             Spacer(),
             Container(
-              color: Colors.white, // Faixa branca
+              color: Colors.white,
               padding: EdgeInsets.all(20),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -50,16 +50,16 @@ class Welcome extends StatelessWidget {
                       );
                     },
                     style: ElevatedButton.styleFrom(
-                      primary: Colors.white, // Cor de fundo branca
-                      onPrimary: Color(0xFF1E88E5), // Cor do texto azul
-                      side: BorderSide(color: Color(0xFF1E88E5)), // Borda azul
+                      primary: Colors.white,
+                      onPrimary: Color(0xFF1E88E5),
+                      side: BorderSide(color: Color(0xFF1E88E5)),
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(20.0),
                       ),
                     ),
                     child: Text('Login'),
                   ),
-                  SizedBox(width: 20), // Espaçamento entre os botões
+                  SizedBox(width: 20),
                   ElevatedButton(
                     onPressed: () {
                       Navigator.push(
@@ -68,9 +68,8 @@ class Welcome extends StatelessWidget {
                       );
                     },
                     style: ElevatedButton.styleFrom(
-                      primary: Color.fromARGB(
-                          255, 90, 175, 249), // Cor de fundo azul
-                      onPrimary: Colors.white, // Cor do texto branco
+                      primary: Color.fromARGB(255, 90, 175, 249),
+                      onPrimary: Colors.white,
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(20.0),
                       ),
