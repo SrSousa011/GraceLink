ec87f1f34a60ee4fef30ac70b6dd1442f3dcd9c4
Lucas Sousa <seu@email.com>
2024-02-23 13:53:02 +0100

    [ADD] Adding HomePage

--- lib/Home.dart
+++ lib/Home.dart
@@ -2,16 +2,92 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
-  const Home({super.key});
+  const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/icons/logo.png',
+              fit: BoxFit.contain,
+              height: 32,
+            ),
+            const SizedBox(width: 10),
+            const Text('Highlights'),
+          ],
+        ),
       ),
-      body: Center(
-        child: Text('Welcome to the Home Screen!'),
+      body: const Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          EventCard(
+            title: 'Culto de Domingo',
+            description:
+                'Participe do culto de domingo para um momento de louvor, adoração e ensino da palavra de Deus.',
+          ),
+          EventCard(
+            title: 'Grupo de Estudo Bíblico',
+            description:
+                'Junte-se ao nosso grupo de estudo bíblico para uma análise profunda das Escrituras.',
+          ),
+          EventCard(
+            title: 'Festa de Boas-Vindas',
+            description:
+                'Conheça novos membros da comunidade em nossa festa de boas-vindas toda quarta-feira.',
+          ),
+          EventCard(
+            title: 'Ação Social',
+            description:
+                'Participe de nossa ação social neste sábado para ajudar os necessitados em nossa comunidade.',
+          ),
+          EventCard(
+            title: 'Noite de Louvor',
+            description:
+                'Desfrute de uma noite de louvor e adoração com músicas inspiradoras e mensagens edificantes.',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class EventCard extends StatelessWidget {
+  final String title;
+  final String description;
+
+  const EventCard({
+    super.key,
+    required this.title,
+    required this.description,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(16),
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              description,
+              style: const TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
       ),
     );
   }
