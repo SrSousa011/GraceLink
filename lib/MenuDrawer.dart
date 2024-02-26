092a2a06cb7d464cf787383a2e29c9c443916b33
Lucas Sousa <seu@email.com>
2024-02-26 15:40:53 +0100

    [ADD] Icons

--- lib/MenuDrawer.dart
+++ lib/MenuDrawer.dart
@@ -23,26 +23,58 @@ class MenuDrawer extends StatelessWidget {
           ),
           ListTile(
             title: const Text('Home'),
+            leading: const Icon(Icons.home),
             onTap: () {
               Navigator.pushReplacementNamed(context, '/Home');
             },
           ),
           ListTile(
             title: const Text('User Profile'),
+            leading: const Icon(Icons.person),
             onTap: () {
               Navigator.pushReplacementNamed(context, '/UserProfile');
             },
           ),
           ListTile(
-            title: const Text('Sign Up'),
+            title: const Text('Events'),
+            leading: const Icon(Icons.event),
             onTap: () {
-              Navigator.pushReplacementNamed(context, '/SignUp');
+              Navigator.pushReplacementNamed(context, '/EventPage');
             },
           ),
           ListTile(
-            title: const Text('Login'),
+            title: const Text('Donations'),
+            leading: const Icon(Icons.volunteer_activism),
             onTap: () {
-              Navigator.pushReplacementNamed(context, '/Login');
+              Navigator.pushReplacementNamed(context, '/donations');
+            },
+          ),
+          ListTile(
+            title: const Text('Courses'),
+            leading: const Icon(Icons.school),
+            onTap: () {
+              Navigator.pushReplacementNamed(context, '/courses');
+            },
+          ),
+          ListTile(
+            title: const Text('Devenir membre'),
+            leading: const Icon(Icons.group_add),
+            onTap: () {
+              Navigator.pushReplacementNamed(context, '/devenir_membre');
+            },
+          ),
+          ListTile(
+            title: const Text('Videos'),
+            leading: const Icon(Icons.video_library),
+            onTap: () {
+              Navigator.pushReplacementNamed(context, '/videos');
+            },
+          ),
+          ListTile(
+            title: const Text('About Us'),
+            leading: const Icon(Icons.info),
+            onTap: () {
+              Navigator.pushReplacementNamed(context, '/about_us');
             },
           ),
         ],
