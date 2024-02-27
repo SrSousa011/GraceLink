29221d4546aab27efdca2f3eac9eb24c6c70af69
Lucas Sousa <seu@email.com>
2024-02-27 16:33:57 +0100

    [EDIT] Menu items using inline ListTile widgets

--- lib/MenuDrawer.dart
+++ lib/MenuDrawer.dart
@@ -18,20 +18,88 @@ class MenuDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
-        padding: EdgeInsets.zero,
+        padding: const EdgeInsets.symmetric(vertical: 24.0),
         children: <Widget>[
-          buildMenuItem('Home', Icons.home_outlined, '/Home', context),
-          buildMenuItem(
-              'User Profile', Icons.person_outlined, '/UserProfile', context),
-          buildMenuItem('Events', Icons.event, '/EventPage', context),
-          buildMenuItem('Donations', Icons.volunteer_activism_outlined,
-              '/donations', context),
-          buildMenuItem('Courses', Icons.school_outlined, '/courses', context),
-          buildMenuItem('Devenir membre', Icons.group_add_outlined,
-              '/devenir_membre', context),
-          buildMenuItem(
-              'Videos', Icons.video_library_outlined, '/videos', context),
-          buildMenuItem('About Us', Icons.info_outlined, '/about_us', context),
+          // Home
+          ListTile(
+            title: const Text('Home'),
+            leading: const Icon(Icons.home_outlined),
+            onTap: () {
+              Navigator.pushReplacementNamed(context, '/Home');
+            },
+          ),
+          // User Profile
+          ListTile(
+            title: const Text('User Profile'),
+            leading: const Icon(Icons.person_outlined),
+            onTap: () {
+              Navigator.pushReplacementNamed(context, '/UserProfile');
+            },
+          ),
+          // Events
+          ListTile(
+            title: const Text('Events'),
+            leading: const Icon(Icons.event),
+            onTap: () {
+              Navigator.pushReplacementNamed(context, '/EventPage');
+            },
+          ),
+          // Donations
+          ListTile(
+            title: const Text('Donations'),
+            leading: const Icon(Icons.volunteer_activism_outlined),
+            onTap: () {
+              Navigator.pushReplacementNamed(context, '/donations');
+            },
+          ),
+          // Courses
+          ListTile(
+            title: const Text('Courses'),
+            leading: const Icon(Icons.school_outlined),
+            onTap: () {
+              Navigator.pushReplacementNamed(context, '/courses');
+            },
+          ),
+          // Devenir membre
+          ListTile(
+            title: const Text('Devenir membre'),
+            leading: const Icon(Icons.group_add_outlined),
+            onTap: () {
+              Navigator.pushReplacementNamed(context, '/devenir_membre');
+            },
+          ),
+          // Videos
+          ListTile(
+            title: const Text('Videos'),
+            leading: const Icon(Icons.video_library_outlined),
+            onTap: () {
+              Navigator.pushReplacementNamed(context, '/videos');
+            },
+          ),
+          // About Us
+          ListTile(
+            title: const Text('About Us'),
+            leading: const Icon(Icons.info_outlined),
+            onTap: () {
+              Navigator.pushReplacementNamed(context, '/about_us');
+            },
+          ),
+          // Notifications
+          ListTile(
+            title: const Text('Notifications'),
+            leading: const Icon(Icons.notifications_outlined),
+            onTap: () {
+              // Handle tap on Notifications menu item
+            },
+          ),
+          // Favorites
+          ListTile(
+            title: const Text('Favorites'),
+            leading: const Icon(Icons.favorite_outline),
+            onTap: () {
+              // Handle tap on Favorites menu item
+            },
+          ),
         ],
       ),
     );
