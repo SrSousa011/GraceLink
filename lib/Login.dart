41ee3a189ccc87ef612da9db618df27a6baec3cc
Lucas Sousa <seu@email.com>
2024-02-27 18:09:28 +0100

    [ADD]  Added GlobalKey to login widget for form interaction

--- lib/Login.dart
+++ lib/Login.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:churchapp/UserProfile.dart';
 import 'package:churchapp/SignUp/SignUp.dart'; // Importa a tela de SignUp
 
-class Login extends StatelessWidget {
-  const Login({Key? key});
+class login extends StatelessWidget {
+  const login({Key? key}) : super(key: key); // Adding a key to the login widget
 
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<FormState> _formKey = GlobalKey<FormState>(); // GlobalKey
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Login'),
@@ -17,68 +19,71 @@ class Login extends StatelessWidget {
           // SingleChildScrollView wrapped around the content
           child: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                  ),
-                ),
-                const SizedBox(height: 20.0),
-                const TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                  ),
-                ),
-                const SizedBox(height: 20.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        // Navigate para a tela de UserProfile
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => UserProfile()),
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor: const Color(0xFF1E88E5),
-                        backgroundColor: Colors.white, // Cor do texto azul
-                        side: const BorderSide(
-                            color: Color(0xFF1E88E5)), // Borda azul
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20.0),
-                        ),
-                      ),
-                      child: const Text('Login'),
+            child: Form(
+              key: _formKey, // Assigning the GlobalKey to the Form widget
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Email',
                     ),
-                    const SizedBox(width: 20), // Espaçamento entre os botões
-                    ElevatedButton(
-                      onPressed: () {
-                        // Navigate para a tela de SignUp
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const SignUp()),
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor: Colors.white,
-                        backgroundColor: const Color.fromARGB(
-                            255, 90, 175, 249), // Cor do texto branco
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20.0),
-                        ),
-                      ),
-                      child: const Text('Sign Up'),
+                  ),
+                  const SizedBox(height: 20.0),
+                  const TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                  const SizedBox(height: 20.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          // Navigate para a tela de UserProfile
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => UserProfile()),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: const Color(0xFF1E88E5),
+                          backgroundColor: Colors.white, // Cor do texto azul
+                          side: const BorderSide(
+                              color: Color(0xFF1E88E5)), // Borda azul
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                        ),
+                        child: const Text('Login'),
+                      ),
+                      const SizedBox(width: 20), // Espaçamento entre os botões
+                      ElevatedButton(
+                        onPressed: () {
+                          // Navigate para a tela de SignUp
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const SignUp()),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor: const Color.fromARGB(
+                              255, 90, 175, 249), // Cor do texto branco
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                        ),
+                        child: const Text('Sign Up'),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
