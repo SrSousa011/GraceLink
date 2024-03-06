810494a5a0725bf48a0bdbf8ad888e64ca1ff9ef
Lucas Sousa <seu@email.com>
2024-03-06 11:51:47 +0100

    [UPDATE] including DonationHow widget and DonationType widget.

--- lib/views/donations/donations.dart
+++ lib/views/donations/donations.dart
@@ -1,7 +1,7 @@
-import 'package:churchapp/views/nav_bar.dart';
+import 'donate_how.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:intl/intl.dart';
+import 'donation_value.dart';
+import 'donation_type.dart';
 
 void main() => runApp(const Donations());
 
@@ -10,109 +10,81 @@ class Donations extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Donations',
+      home: DonationPage(),
+    );
+  }
+}
+
+class DonationPage extends StatefulWidget {
+  const DonationPage({Key? key}) : super(key: key);
+
+  @override
+  _DonationPageState createState() => _DonationPageState();
+}
+
+class _DonationPageState extends State<DonationPage> {
+  late String donationType;
+  late String selectedPayment;
+  late TextEditingController donationController;
+
+  @override
+  void initState() {
+    super.initState();
+    donationController = TextEditingController();
+    donationType = '';
+    selectedPayment = 'Paypal';
+  }
+
+  @override
+  void dispose() {
+    donationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Donations'),
       ),
-      drawer: const NavBar(),
-      body: SingleChildScrollView(
-        padding:
-            EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-        child: const DonationWidget(),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            DonationValue(
+              donationController: donationController,
+            ),
+            const SizedBox(height: 20.0),
+            DonationHow(
+              selectedPayment: selectedPayment,
+              onPaymentSelected: (payment) {
+                setState(() {
+                  selectedPayment = payment!;
+                });
+              },
+              onTypeSelected: (type) {
+                setState(() {
+                  donationType = type!;
+                });
+              },
+            ),
+            DonationType(
+              onTypeSelected: (type) {
+                setState(() {
+                  donationType = type!;
+                });
+              },
+              donationType: '',
+            ),
+            const SizedBox(height: 70.0),
+            if (donationType.isNotEmpty) // Display selected donation type image
+              Image.asset('assets/$donationType.png'),
+          ],
+        ),
       ),
-    );
-  }
-}
-
-class DonationWidget extends StatefulWidget {
-  const DonationWidget({Key? key}) : super(key: key);
-
-  @override
-  State<DonationWidget> createState() => _DonationWidgetState();
-}
-
-class _DonationWidgetState extends State<DonationWidget> {
-  TextEditingController donationController = TextEditingController();
-  String? donationType;
-  String selectedPayment = 'Paypal'; // Default selected payment
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          const Text(
-            'Valor da doação:',
-            style: TextStyle(
-              fontSize: 24.0,
-              fontWeight: FontWeight.bold,
-              color: Color.fromARGB(255, 153, 153, 153),
-            ),
-          ),
-          TextField(
-            controller: donationController,
-            keyboardType: TextInputType.number,
-            inputFormatters: [
-              FilteringTextInputFormatter.digitsOnly,
-              CurrencyInputFormatter()
-            ],
-            decoration: const InputDecoration(
-              hintText: 'Digite o valor da sua doação',
-            ),
-          ),
-          const SizedBox(height: 20.0),
-          const Text(
-            'Doar como:',
-            style: TextStyle(
-              fontSize: 24.0,
-              fontWeight: FontWeight.bold,
-              color: Color.fromARGB(255, 153, 153, 153),
-            ),
-          ),
-          const SizedBox(height: 10.0),
-          DropdownButtonFormField<String>(
-            value: selectedPayment,
-            onChanged: (String? newValue) {
-              setState(() {
-                selectedPayment = newValue!;
-              });
-            },
-            items: <String>['Paypal', 'Cartão de Crédito', 'Cartão de Débito']
-                .map<DropdownMenuItem<String>>((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(value),
-              );
-            }).toList(),
-            decoration: const InputDecoration(
-              labelText: ' ',
-            ),
-          ),
-          const SizedBox(height: 70.0),
-          if (donationType != null) Image.asset('assets/$donationType.png'),
-        ],
-      ),
-    );
-  }
-}
-
-class CurrencyInputFormatter extends TextInputFormatter {
-  @override
-  TextEditingValue formatEditUpdate(
-      TextEditingValue oldValue, TextEditingValue newValue) {
-    if (newValue.selection.baseOffset == 0) {
-      return newValue;
-    }
-
-    double value = double.parse(newValue.text);
-    final formatter = NumberFormat.currency(locale: 'pt_BR', symbol: '€');
-    String newText = formatter.format(value / 100);
-
-    return newValue.copyWith(
-      text: newText,
-      selection: TextSelection.collapsed(offset: newText.length),
     );
   }
 }
