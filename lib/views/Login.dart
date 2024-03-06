f905335c618ca2f6bc6fd8bc364eb8bb1b445b74
Lucas Sousa <seu@email.com>
2024-03-06 10:09:38 +0100

    [EDIT] Fixing imports

--- lib/views/Login.dart
+++ lib/views/Login.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: file_names, library_private_types_in_public_api
 
-import 'package:churchapp/views/SignUp/signUp.dart';
+import 'package:churchapp/views/signUp/signUp.dart';
 import 'package:churchapp/views/user_profile.dart';
 import 'package:flutter/material.dart'; // Importa a tela de SignUp
 
