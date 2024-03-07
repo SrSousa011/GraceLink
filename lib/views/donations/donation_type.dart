387bd8240ecd118c0b1b817c815ed78ed0b43a08
Lucas Sousa <seu@email.com>
2024-03-07 10:03:13 +0100

    [EDIT] Add a label for the check buttons

--- lib/views/donations/donation_type.dart
+++ lib/views/donations/donation_type.dart
@@ -21,9 +21,24 @@ class _DonationTypeState extends State<DonationType> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
+          const Text(
+            'Tipo de Doação:', // Adicionando rótulo
+            style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(255, 153, 153, 153),
+            ),
+          ),
           ElevatedButton.icon(
             onPressed: () {
-              _showDonationDialog();
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DonationOptionsScreen(
+                    onTypeSelected: widget.onTypeSelected,
+                  ),
+                ),
+              );
             },
             icon: const Icon(Icons.attach_money),
             label: const Text("Escolha o tipo de doação"),
@@ -31,7 +46,14 @@ class _DonationTypeState extends State<DonationType> {
           const SizedBox(height: 20), // Espaçamento entre os botões
           ElevatedButton.icon(
             onPressed: () {
-              _showFoodDialog();
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => FoodOptionsScreen(
+                    onTypeSelected: widget.onTypeSelected,
+                  ),
+                ),
+              );
             },
             icon: const Icon(Icons.food_bank),
             label: const Text("Selecione o tipo de alimento"),
@@ -40,72 +62,80 @@ class _DonationTypeState extends State<DonationType> {
       ),
     );
   }
+}
 
-  void _showDonationDialog() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text("Escolha o tipo de doação"),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              InkWell(
-                onTap: () {
-                  Navigator.of(context).pop();
-                  widget.onTypeSelected("Dizimo");
-                },
-                child: const ListTile(
-                  title: Text("Dízimo"),
-                ),
-              ),
-              InkWell(
-                onTap: () {
-                  Navigator.of(context).pop();
-                  widget.onTypeSelected("Oferta");
-                },
-                child: const ListTile(
-                  title: Text("Oferta"),
-                ),
-              ),
-            ],
+class DonationOptionsScreen extends StatelessWidget {
+  final void Function(String) onTypeSelected;
+
+  const DonationOptionsScreen({Key? key, required this.onTypeSelected})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Escolha o tipo de doação"),
+      ),
+      body: Column(
+        children: <Widget>[
+          InkWell(
+            onTap: () {
+              onTypeSelected("Dízimo");
+              Navigator.pop(context); // Close the current screen
+            },
+            child: const ListTile(
+              title: Text("Dízimo"),
+            ),
           ),
-        );
-      },
+          InkWell(
+            onTap: () {
+              onTypeSelected("Oferta");
+              Navigator.pop(context); // Close the current screen
+            },
+            child: const ListTile(
+              title: Text("Oferta"),
+            ),
+          ),
+        ],
+      ),
     );
   }
+}
 
-  void _showFoodDialog() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text("Selecione o tipo de alimento"),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              InkWell(
-                onTap: () {
-                  Navigator.of(context).pop();
-                  widget.onTypeSelected("Projeto doar e amar");
-                },
-                child: const ListTile(
-                  title: Text("Projeto doar e amar"),
-                ),
-              ),
-              InkWell(
-                onTap: () {
-                  Navigator.of(context).pop();
-                  widget.onTypeSelected("Missao Africa");
-                },
-                child: const ListTile(
-                  title: Text("Missão África"),
-                ),
-              ),
-            ],
+class FoodOptionsScreen extends StatelessWidget {
+  final void Function(String) onTypeSelected;
+
+  const FoodOptionsScreen({Key? key, required this.onTypeSelected})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Selecione o tipo de alimento"),
+      ),
+      body: Column(
+        children: <Widget>[
+          InkWell(
+            onTap: () {
+              onTypeSelected("Projeto doar e amar");
+              Navigator.pop(context); // Close the current screen
+            },
+            child: const ListTile(
+              title: Text("Projeto doar e amar"),
+            ),
           ),
-        );
-      },
+          InkWell(
+            onTap: () {
+              onTypeSelected("Missão África");
+              Navigator.pop(context); // Close the current screen
+            },
+            child: const ListTile(
+              title: Text("Missão África"),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
