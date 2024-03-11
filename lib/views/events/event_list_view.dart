1d52a81a838ca71da81662d4ff420b88972f6aaf
Lucas Sousa <seu@email.com>
2024-03-11 12:24:48 +0100

    [ADD] Screen for adding a new event

---
+++ lib/views/events/event_list_view.dart
@@ -0,0 +1,32 @@
+import 'package:churchapp/views/events/events.dart';
+import 'package:flutter/material.dart';
+
+class EventListView extends StatelessWidget {
+  final List<Event> events;
+  final void Function(Event) onTap;
+
+  const EventListView({
+    Key? key,
+    required this.events,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: events.length,
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          onTap: () => onTap(events[index]),
+          child: EventCard(
+            title: events[index].title,
+            description: events[index].description,
+            date: events[index].date,
+            time: events[index].time,
+            location: events[index].location,
+          ),
+        );
+      },
+    );
+  }
+}
