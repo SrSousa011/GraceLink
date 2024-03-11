372a85e850515960f81c0b4ed948d0d1409f78ba
Lucas Sousa <seu@email.com>
2024-03-11 10:01:28 +0100

    [ADD] Updated BecomeMember screen with country code dropdown and European country options

--- lib/views/Member/become_member.dart
+++ lib/views/Member/become_member.dart
@@ -1,5 +1,17 @@
-import 'package:churchapp/views/nav_bar.dart';
+import 'package:country_icons/country_icons.dart';
 import 'package:flutter/material.dart';
+
+class DDDCountryItem {
+  final String countryCode;
+  final String dddCode;
+  final int numberOfDigits; // New field for the number of digits
+
+  DDDCountryItem({
+    required this.countryCode,
+    required this.dddCode,
+    required this.numberOfDigits,
+  });
+}
 
 class BecomeMember extends StatefulWidget {
   const BecomeMember({Key? key}) : super(key: key);
@@ -11,126 +23,209 @@ class BecomeMember extends StatefulWidget {
 class _BecomeMemberState extends State<BecomeMember> {
   TextEditingController emailController = TextEditingController();
   TextEditingController phoneController = TextEditingController();
-  TextEditingController dddController = TextEditingController();
   TextEditingController churchController = TextEditingController();
   TextEditingController maritalStatusController = TextEditingController();
   TextEditingController reasonController = TextEditingController();
   TextEditingController referenceController = TextEditingController();
 
   String selectedCivilState = 'Single';
+  String selectedDDD = '+1';
+  int numberOfPhoneDigits = 10; // Default number of digits
+
+  List<DDDCountryItem> dddCountryList = [
+    DDDCountryItem(
+      countryCode: 'US',
+      dddCode: '+1',
+      numberOfDigits: 10,
+    ),
+    DDDCountryItem(
+      countryCode: 'BR',
+      dddCode: '+55',
+      numberOfDigits: 11,
+    ),
+    DDDCountryItem(
+      countryCode: 'UK',
+      dddCode: '+44',
+      numberOfDigits: 11,
+    ),
+    DDDCountryItem(
+      countryCode: 'FR',
+      dddCode: '+33',
+      numberOfDigits: 9,
+    ),
+    DDDCountryItem(
+      countryCode: 'DE',
+      dddCode: '+49',
+      numberOfDigits: 11,
+    ),
+    DDDCountryItem(
+      countryCode: 'ES',
+      dddCode: '+34',
+      numberOfDigits: 9,
+    ),
+    DDDCountryItem(
+      countryCode: 'IT',
+      dddCode: '+39',
+      numberOfDigits: 10,
+    ),
+    DDDCountryItem(
+      countryCode: 'LU',
+      dddCode: '+352',
+      numberOfDigits: 9,
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Tornar-se Membro'),
-        centerTitle: true,
-      ),
-      drawer: const NavBar(),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              controller: emailController,
-              decoration: const InputDecoration(labelText: 'Adress'),
-            ),
-            const SizedBox(height: 20.0),
-            Row(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: TextField(
-                    controller: dddController,
-                    keyboardType: TextInputType.number,
-                    decoration: const InputDecoration(labelText: 'DDD'),
-                  ),
-                ),
-                const SizedBox(width: 10.0),
-                Expanded(
-                  flex: 3,
-                  child: TextField(
-                    controller: phoneController,
-                    keyboardType: TextInputType.number,
-                    decoration: const InputDecoration(labelText: 'Telefone'),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 20.0),
-            TextField(
-              controller: churchController,
-              decoration:
-                  const InputDecoration(labelText: 'Última igreja visitada?'),
-            ),
-            const SizedBox(height: 20.0),
-            DropdownButtonFormField<String>(
-              decoration: const InputDecoration(
-                labelText: 'Estado Civil?',
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Tornar-se Membro'),
+          centerTitle: true,
+        ),
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                controller: emailController,
+                decoration: const InputDecoration(labelText: 'Address'),
               ),
-              value: selectedCivilState,
-              onChanged: (value) {
-                setState(() {
-                  selectedCivilState = value!;
-                });
-              },
-              items: const [
-                DropdownMenuItem<String>(
-                  value: 'Single',
-                  child: Text('Single'),
-                ),
-                DropdownMenuItem<String>(
-                  value: 'Married',
-                  child: Text('Married'),
-                ),
-                DropdownMenuItem<String>(
-                  value: 'Divorced',
-                  child: Text('Divorced'),
-                ),
-              ],
-            ),
-            const SizedBox(height: 20.0),
-            TextField(
-              controller: reasonController,
-              decoration: const InputDecoration(
-                  labelText: 'Porque deseja se tornar um membro?'),
-            ),
-            const SizedBox(height: 20.0),
-            TextField(
-              controller: referenceController,
-              decoration: const InputDecoration(
-                  labelText: 'Referência de um membro atual'),
-            ),
-            const SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      title: const Text('Confirmação'),
-                      content:
-                          const Text('Você tornou-se um membro com sucesso!'),
-                      actions: <Widget>[
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: const Text('Fechar'),
+              const SizedBox(height: 20.0),
+              Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: DropdownButtonFormField<String>(
+                            decoration: const InputDecoration(
+                              labelText: 'Country Code',
+                            ),
+                            value: selectedDDD,
+                            onChanged: (value) {
+                              setState(() {
+                                selectedDDD = value!;
+                                // Update the number of phone digits based on the selected country
+                                numberOfPhoneDigits = dddCountryList
+                                    .firstWhere(
+                                        (item) => item.dddCode == selectedDDD)
+                                    .numberOfDigits;
+                              });
+                            },
+                            items: dddCountryList.map((item) {
+                              return DropdownMenuItem<String>(
+                                value: item.dddCode,
+                                child: Row(
+                                  children: [
+                                    const SizedBox(width: 10),
+                                    Text(item.dddCode),
+                                  ],
+                                ),
+                              );
+                            }).toList(),
+                          ),
+                        ),
+                        const SizedBox(width: 10.0),
+                        Expanded(
+                          flex: 3,
+                          child: TextField(
+                            controller: phoneController,
+                            keyboardType: TextInputType.number,
+                            decoration: const InputDecoration(
+                              labelText: 'Phone',
+                            ),
+                            maxLength: numberOfPhoneDigits,
+                          ),
                         ),
                       ],
-                    );
-                  },
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.white,
-                backgroundColor: Colors.blue, // Text color white
+                    ),
+                  ),
+                ],
               ),
-              child: const Text('Tornar-se Membro'),
-            ),
-          ],
+              const SizedBox(height: 20.0),
+              TextField(
+                controller: churchController,
+                decoration:
+                    const InputDecoration(labelText: 'Last visited church?'),
+              ),
+              const SizedBox(height: 20.0),
+              DropdownButtonFormField<String>(
+                decoration: const InputDecoration(
+                  labelText: 'Civil State',
+                ),
+                value: selectedCivilState,
+                onChanged: (value) {
+                  setState(() {
+                    selectedCivilState = value!;
+                  });
+                },
+                items: const [
+                  DropdownMenuItem<String>(
+                    value: 'Single',
+                    child: Text('Single'),
+                  ),
+                  DropdownMenuItem<String>(
+                    value: 'Married',
+                    child: Text('Married'),
+                  ),
+                  DropdownMenuItem<String>(
+                    value: 'Divorced',
+                    child: Text('Divorced'),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20.0),
+              TextField(
+                controller: reasonController,
+                decoration: const InputDecoration(
+                  labelText: 'Why do you want to become a member?',
+                ),
+              ),
+              const SizedBox(height: 20.0),
+              TextField(
+                controller: referenceController,
+                decoration: const InputDecoration(
+                  labelText: 'Reference from a current member',
+                ),
+              ),
+              const SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: const Text('Confirmation'),
+                        content: const Text(
+                            'You have successfully become a member!'),
+                        actions: <Widget>[
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: const Text('Close'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.blue,
+                ),
+                child: const Text('Become a Member'),
+              ),
+            ],
+          ),
         ),
       ),
     );
