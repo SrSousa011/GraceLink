f62820ef5d6a0b1817dd18e83df9385fe4a12890
Lucas Sousa <seu@email.com>
2024-03-12 12:37:30 +0100

    [ADD] package push notification

--- lib/views/notifications/firebase_api.dart
+++
@@ -1,7 +0,0 @@
-class FirebaseApi {
-  final _firebaseMessaging = FirebaseMessaging.instance;
-
-  Future<void> initNotifications() async {
-    await _firebaseMessaging.requestPermission();
-  }
-}
