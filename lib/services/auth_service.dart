88ce763edd8ae17df613a7afa98ad68d6ca7bd05
Lucas Sousa <seu@email.com>
2024-03-27 16:47:25 +0100

    [UPDATE] Refactored AuthenticationService to implement BaseAuth interface and added new methods for signing up.

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,14 +1,48 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 
-class AuthenticationService {
+abstract class BaseAuth {
+  Future<UserCredential> createUserWithEmailAndPassword(
+      {required String email, required String password});
+
+  Future<UserCredential> signInWithEmailAndPassword(
+      {required String email, required String password});
+
+  Future<String?> getCurrentUserId();
+
+  Future<void> signOut();
+
+  Future<bool> isLoggedIn();
+
+  // New methods for signing up
+  Future<void> signUpWithPersonalInfo({
+    required String firstName,
+    required String lastName,
+  });
+
+  Future<void> signUpWithDateOfBirthAndGender({
+    required int? selectedDay,
+    required int? selectedMonth,
+    required int? selectedYear,
+    required String selectedGender,
+  });
+
+  Future<void> signUpWithEmailAndConfirmation({
+    required String email,
+    required String emailConfirmation,
+  });
+}
+
+class AuthenticationService implements BaseAuth {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  @override
   Future<void> signUpWithPersonalInfo({
     required String firstName,
     required String lastName,
   }) async {}
 
+  @override
   Future<void> signUpWithDateOfBirthAndGender({
     required int? selectedDay,
     required int? selectedMonth,
@@ -16,6 +50,7 @@ class AuthenticationService {
     required String selectedGender,
   }) async {}
 
+  @override
   Future<void> signUpWithEmailAndConfirmation({
     required String email,
     required String emailConfirmation,
@@ -45,5 +80,71 @@ class AuthenticationService {
       }
       rethrow;
     }
+  }
+
+  @override
+  Future<UserCredential> createUserWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      return await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      rethrow;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  @override
+  Future<UserCredential> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      return await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      rethrow;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  @override
+  Future<String?> getCurrentUserId() async {
+    try {
+      User? user = _auth.currentUser;
+      return user?.uid;
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error getting current user ID: $e');
+      }
+      rethrow;
+    }
+  }
+
+  @override
+  Future<void> signOut() async {
+    // Implement signOut method
+    try {
+      await _auth.signOut();
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error signing out: $e');
+      }
+      rethrow;
+    }
+  }
+
+  @override
+  Future<bool> isLoggedIn() async {
+    User? user = _auth.currentUser;
+    return user != null;
   }
 }
