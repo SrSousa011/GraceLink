61e21aa467353cb06c0bdef5f5a4f11dbfc27554
Lucas Sousa <seu@email.com>
2024-03-27 16:46:02 +0100

    [UPDATE] Refactored main.dart to use Root widget for authentication flow

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,8 @@
-import 'package:churchapp/views/home/home.dart';
-import 'package:churchapp/views/member/become_member.dart';
-import 'package:churchapp/views/about_us.dart';
-import 'package:churchapp/views/nav_bar.dart';
-import 'package:churchapp/views/courses/courses.dart';
-import 'package:churchapp/views/donations/donations.dart';
-import 'package:churchapp/views/signUp/sign_up_page.dart';
+import 'package:churchapp/route/root.dart';
 import 'package:flutter/material.dart';
-import 'package:churchapp/views/login.dart';
+import 'package:churchapp/services/auth_service.dart';
+import 'package:churchapp/views/nav_bar.dart';
 import 'package:churchapp/views/splash_screen.dart';
-import 'package:churchapp/views/user_profile.dart';
-import 'package:churchapp/views/events/events.dart';
-import 'package:churchapp/views/welcome.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
@@ -29,28 +21,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Comunidade Connect',
-      // Removendo o tema para que o aplicativo use o tema padrão do Flutter
-      // theme: ThemeData(
-      //   colorScheme: ColorScheme.fromSwatch(
-      //     primarySwatch: Colors.blue, // Adjust as needed
-      //   ),
-      //   // useMaterial3: true, // Remove this line if not needed
-      // ),
-      home: const SplashScreen(
-        drawer: NavBar(),
+      home: SplashScreen(
+        root: Root(auth: AuthenticationService()), // Using the Root widget
+        drawer: const NavBar(),
       ),
-      routes: {
-        '/login': (context) => const SignInPage(),
-        '/signUp': (context) => const SignUpPage(),
-        '/home': (context) => const Home(),
-        '/courses': (context) => const Courses(),
-        '/user_profile': (context) => const UserProfile(),
-        '/event_page': (context) => const Events(),
-        '/donations': (context) => const Donations(),
-        '/about_us': (context) => const AboutUs(),
-        '/become_member': (context) => const BecomeMember(),
-        '/welcome': (context) => const Welcome(title: 'GraceLink'),
-      },
     );
   }
 }
