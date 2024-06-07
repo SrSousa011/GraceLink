faa4e803509a386dd38aa1ea50048bb98c457114
Lucas Sousa <seu@email.com>
2024-06-07 12:26:44 +0200

    [ADD] Description, date of new events

--- lib/views/events/events.dart
+++ lib/views/events/events.dart
@@ -93,18 +93,54 @@ class EventListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin: const EdgeInsets.all(8.0),
-      child: ListTile(
-        title: Text(event.title),
-        subtitle: Text(DateFormat('dd/MM/yyyy').format(event.date)),
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => EventDetailsScreen(event: event)),
-          );
-        },
+      margin: const EdgeInsets.all(8.0), // Location: EventListItem
+      child: buildListTile(context),
+    );
+  }
+
+  Widget buildListTile(BuildContext context) {
+    return ListTile(
+      title: Text(
+        event.title,
+        style: const TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 18.0,
+        ),
       ),
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            DateFormat('dd/MM/yyyy').format(event.date),
+            style: const TextStyle(
+              fontStyle: FontStyle.italic,
+              fontSize: 14.0,
+            ),
+          ),
+          const SizedBox(height: 4), // Spacer between date and description
+          Text(
+            event.description,
+            maxLines: 2, // Adjust as needed
+            overflow: TextOverflow.ellipsis,
+          ),
+          const SizedBox(height: 4), // Spacer between description and location
+          Text(
+            event.location,
+            style: TextStyle(
+              color: Colors.grey[600],
+              fontSize: 12.0,
+            ),
+          ),
+        ],
+      ),
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => EventDetailsScreen(event: event),
+          ),
+        );
+      },
     );
   }
 }
