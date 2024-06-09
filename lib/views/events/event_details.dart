78acea48a6a255e104e1cdfc8a7a6c1bec8228d6
Lucas Sousa <seu@email.com>
2024-06-09 11:04:48 +0200

    [ADD] Events details

---
+++ lib/views/events/event_details.dart
@@ -0,0 +1,97 @@
+import 'package:churchapp/views/events/events.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class EventDetailsScreen extends StatelessWidget {
+  final Event event;
+
+  const EventDetailsScreen({super.key, required this.event});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(event.title),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.edit),
+            onPressed: () {
+              _navigateToUpdateEventScreen(context, event);
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.delete),
+            onPressed: () {
+              _deleteEvent(context, event);
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Título: ${event.title}',
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            _buildDetailsText('Descrição: ${event.description}'),
+            _buildDetailsText(
+              'Data: ${DateFormat('dd/MM/yyyy').format(event.date)}',
+            ),
+            _buildDetailsText('Hora: ${event.time.format(context)}'),
+            _buildDetailsText('Localização: ${event.location}'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDetailsText(String text) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Text(
+        text,
+        style: const TextStyle(fontSize: 16),
+      ),
+    );
+  }
+
+  void _navigateToUpdateEventScreen(BuildContext context, Event event) async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => UpdateEventForm(event: event)),
+    );
+    if (result != null && result) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Evento atualizado')),
+      );
+    }
+  }
+
+  void _deleteEvent(BuildContext context, Event event) async {
+    try {
+      await deleteEvent(event.id);
+      Navigator.pop(context, true); // Return true to indicate success
+    } catch (e) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Erro ao excluir evento'),
+            content: const Text('Ocorreu um erro ao tentar excluir o evento.'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+}
