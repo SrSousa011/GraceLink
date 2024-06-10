eff092805fe5deb883445c894a6ceb7ef077a03f
Lucas Sousa <seu@email.com>
2024-06-10 16:28:45 +0200

    [ADD] Snackbar Util

--- lib/_comun/snackbar_util.dart
+++ lib/_comun/snackbar_util.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
 class SnackBarUtil {
-  static void displaySnackBar(BuildContext context, String message) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text(message),
-      ),
-    );
+  static void displaySnackBar(
+    BuildContext context,
+    String message, {
+    bool isError = false,
+  }) {
+    if (context.findRenderObject()!.attached) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(message),
+          backgroundColor: isError ? Colors.red : Colors.green,
+        ),
+      );
+    }
   }
 }
