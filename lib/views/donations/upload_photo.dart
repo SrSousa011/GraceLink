4e38c8bf70dba99f406b5074e7aea597ae4b05de
Lucas Sousa <seu@email.com>
2024-06-10 11:51:01 +0200

    [ADD]  Firebase Storage integration for image upload in StoragePage

--- lib/views/donations/upload_photo.dart
+++ lib/views/donations/upload_photo.dart
@@ -1,26 +1,162 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class UploadPhotoScreen extends StatelessWidget {
-  const UploadPhotoScreen({super.key});
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+class StoragePage extends StatefulWidget {
+  const StoragePage({super.key});
+
+  @override
+  State<StoragePage> createState() => _StoragePageState();
+}
+
+class _StoragePageState extends State<StoragePage> {
+  final FirebaseStorage firebaseStorage = FirebaseStorage.instance;
+  final ImagePicker _picker = ImagePicker();
+  Reference? _uploadedImageRef;
+  String? _uploadedImageUrl;
+  bool _uploading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadImage();
+  }
+
+  Future<void> _loadImage() async {
+    try {
+      ListResult result = await firebaseStorage.ref('images').listAll();
+      if (result.items.isNotEmpty) {
+        // Load only the first image if exists
+        _uploadedImageRef = result.items[0];
+        _uploadedImageUrl = await _uploadedImageRef!.getDownloadURL();
+      } else {
+        _uploadedImageRef = null;
+        _uploadedImageUrl = null;
+      }
+      setState(() {
+        _uploading = false;
+      });
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error loading image: $e');
+      }
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Error loading image')),
+      );
+      setState(() {
+        _uploading = false;
+      });
+    }
+  }
+
+  Future<void> _uploadImage() async {
+    XFile? file = await _picker.pickImage(source: ImageSource.gallery);
+    if (file != null) {
+      try {
+        File uploadFile = File(file.path);
+        String ref = 'images/img-${DateTime.now().millisecondsSinceEpoch}.jpg';
+        UploadTask uploadTask = firebaseStorage.ref(ref).putFile(uploadFile);
+
+        await uploadTask.whenComplete(() => _loadImage());
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('File Uploaded Successfully')),
+        );
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Upload Error')),
+        );
+      }
+    }
+  }
+
+  Future<void> _removeImage() async {
+    try {
+      if (_uploadedImageRef != null) {
+        await _uploadedImageRef!.delete();
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('File Removed Successfully')),
+        );
+        setState(() {
+          _uploadedImageRef = null;
+          _uploadedImageUrl = null;
+        });
+      }
+    } catch (e) {
+      print('Error removing image: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Error removing image')),
+      );
+    }
+  }
+
+  Future<void> _confirmAndNavigateBack() async {
+    // Here you can perform any confirmation logic
+    // For this example, let's just navigate back
+    Navigator.of(context).pop();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Upload Photo'),
+        title: const Text('Firebase Storage'),
+        elevation: 0,
+        actions: [
+          if (_uploading)
+            const Padding(
+              padding: EdgeInsets.only(right: 12.0),
+              child: Center(
+                child: SizedBox(
+                  width: 20,
+                  height: 20,
+                  child: CircularProgressIndicator.adaptive(
+                    strokeWidth: 3,
+                    valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                  ),
+                ),
+              ),
+            ),
+          IconButton(
+            icon: const Icon(Icons.upload),
+            onPressed:
+                _uploading || _uploadedImageUrl != null ? null : _uploadImage,
+          ),
+        ],
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Implement photo upload logic
-            // For now, just navigate back to the previous screen
-            Navigator.pop(context);
-          },
-          style: ElevatedButton.styleFrom(
-            foregroundColor: Colors.white,
-            backgroundColor: const Color(0xFF5AAFf9),
-          ),
-          child: const Text('Upload Photo'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            if (_uploadedImageUrl != null)
+              Card(
+                margin: const EdgeInsets.all(8.0),
+                child: ListTile(
+                  title: Image.network(_uploadedImageUrl!),
+                  trailing: Wrap(
+                    spacing: 12, // space between each button
+                    children: <Widget>[
+                      ElevatedButton(
+                        onPressed: _confirmAndNavigateBack,
+                        child: const Text('Confirm'),
+                      ),
+                      ElevatedButton(
+                        onPressed: _removeImage,
+                        child: const Text('Remove Image'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            if (_uploading)
+              const Padding(
+                padding: EdgeInsets.all(16.0),
+                child: CircularProgressIndicator(),
+              ),
+          ],
         ),
       ),
     );
