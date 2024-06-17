e568551ac5986c44edcaa0d0ad2d585b04d58635
Lucas Sousa <seu@email.com>
2024-06-17 12:46:57 +0200

    [UPDATE] Refactor Root widget to use StreamBuilder for authentication

--- lib/route/root.dart
+++ lib/route/root.dart
@@ -1,115 +1,41 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:churchapp/provider/user_provider.dart';
 import 'package:churchapp/services/auth_service.dart';
-import 'package:churchapp/views/login/login.dart';
 import 'package:churchapp/views/home/home.dart';
+import 'package:churchapp/views/login/login.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class Root extends StatefulWidget {
+class Root extends StatelessWidget {
+  final AuthenticationService auth;
   const Root({super.key, required this.auth});
-
-  final BaseAuth auth;
-
-  @override
-  State<StatefulWidget> createState() => _RootState();
-}
-
-enum AuthStatus {
-  notDetermined,
-  notLoggedIn,
-  loggedIn,
-}
-
-class _RootState extends State<Root> {
-  AuthStatus _authStatus = AuthStatus.notDetermined;
-  late String _currentUserId;
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-
-  @override
-  void dispose() {
-    _emailController.dispose();
-    _passwordController.dispose();
-    super.dispose();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _checkLoginStatus();
-  }
-
-  void _checkLoginStatus() async {
-    try {
-      bool isLoggedIn = await widget.auth.isLoggedIn();
-      if (mounted) {
-        setState(() {
-          _authStatus =
-              isLoggedIn ? AuthStatus.loggedIn : AuthStatus.notLoggedIn;
-        });
-      }
-    } catch (e) {
-      if (kDebugMode) {
-        print('Error checking login status: $e');
-      }
-      if (mounted) {
-        setState(() {
-          _authStatus = AuthStatus.notLoggedIn;
-        });
-      }
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
-    switch (_authStatus) {
-      case AuthStatus.notDetermined:
-        return _buildLoadingScreen();
-      case AuthStatus.notLoggedIn:
-        return Login(
-          auth: widget.auth,
-          onLoggedIn: _handleLoggedIn,
-        );
-      case AuthStatus.loggedIn:
-        return _buildHomeScreen();
-    }
-  }
-
-  Widget _buildLoadingScreen() {
-    return const Scaffold(
-      body: Center(
-        child: CircularProgressIndicator(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => UserProvider()),
+      ],
+      child: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Scaffold(
+              body: Center(child: CircularProgressIndicator()),
+            );
+          } else if (snapshot.hasData) {
+            return Home(
+              auth: auth,
+              userId: snapshot.data!.uid,
+              onSignedOut: () {},
+            );
+          } else {
+            return Login(
+              auth: auth,
+            );
+          }
+        },
       ),
     );
-  }
-
-  Widget _buildHomeScreen() {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(
-          builder: (context) => Home(
-            auth: widget.auth,
-            userId: _currentUserId,
-            onSignedOut: _handleSignedOut,
-          ),
-        ),
-      );
-    });
-    return _buildLoadingScreen();
-  }
-
-  void _handleLoggedIn() {
-    if (mounted) {
-      setState(() {
-        _authStatus = AuthStatus.loggedIn;
-      });
-    }
-  }
-
-  void _handleSignedOut() {
-    if (mounted) {
-      setState(() {
-        _authStatus = AuthStatus.notLoggedIn;
-      });
-    }
   }
 }
