b690c519d8a4ea1a2193266260c2a686d0237301
Lucas Sousa <seu@email.com>
2024-07-04 19:12:06 +0200

    [ADD] Drawer menu item

--- lib/views/nav_bar/drawer_menu_item.dart
+++ lib/views/nav_bar/drawer_menu_item.dart
@@ -1,3 +1,4 @@
+import 'package:churchapp/views/user_Profile/user_profile.dart';
 import 'package:flutter/material.dart';
 
 class DrawerMenuItem extends StatelessWidget {
@@ -21,6 +22,61 @@ class DrawerMenuItem extends StatelessWidget {
         Navigator.pop(context); // Close the drawer before navigating
         Navigator.pushReplacementNamed(context, route);
       },
+    );
+  }
+}
+
+class DrawerHeaderWidget extends StatelessWidget {
+  final String? fullName;
+
+  const DrawerHeaderWidget({
+    super.key,
+    required this.fullName,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 270,
+      child: DrawerHeader(
+        decoration: const BoxDecoration(
+          color: Colors.blue,
+          image: DecorationImage(
+            image: AssetImage('assets/imagens/bacground-image-center.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: GestureDetector(
+          onTap: () {
+            Navigator.pop(context); // Close the drawer before navigating
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const UserProfile()),
+            );
+          },
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 72,
+                backgroundColor:
+                    Colors.grey[200], // Cor de fundo do CircleAvatar
+                child: const Icon(
+                  Icons.account_circle,
+                  size: 144, // Tamanho do ícone dentro do CircleAvatar
+                  color: Colors.grey, // Cor do ícone
+                ),
+              ),
+              const SizedBox(height: 12),
+              Text(
+                fullName ?? 'Loading...',
+                style: const TextStyle(fontSize: 20, color: Colors.white),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/views/nav_bar/nav_bar.dart
+++ lib/views/nav_bar/nav_bar.dart
@@ -1,4 +1,4 @@
-import 'package:churchapp/views/user_Profile/user_profile.dart';
+import 'package:churchapp/views/nav_bar/drawer_menu_item.dart';
 import 'package:flutter/material.dart';
 import 'package:churchapp/services/auth_service.dart';
 import 'package:churchapp/theme/welcome.dart';
@@ -112,86 +112,6 @@ class _NavBarState extends State<NavBar> {
             },
           ),
         ],
-      ),
-    );
-  }
-}
-
-class DrawerMenuItem extends StatelessWidget {
-  const DrawerMenuItem({
-    super.key,
-    required this.title,
-    required this.icon,
-    required this.route,
-  });
-
-  final String title;
-  final IconData icon;
-  final String route;
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(title),
-      leading: Icon(icon),
-      onTap: () {
-        Navigator.pop(context); // Close the drawer before navigating
-        Navigator.pushReplacementNamed(context, route);
-      },
-    );
-  }
-}
-
-class DrawerHeaderWidget extends StatelessWidget {
-  final String? fullName;
-
-  const DrawerHeaderWidget({
-    super.key,
-    required this.fullName,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      height: 270,
-      child: DrawerHeader(
-        decoration: const BoxDecoration(
-          color: Colors.blue,
-          image: DecorationImage(
-            image: AssetImage('assets/imagens/bacground-image-center.jpg'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: GestureDetector(
-          onTap: () {
-            Navigator.pop(context); // Close the drawer before navigating
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const UserProfile()),
-            );
-          },
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                radius: 72,
-                backgroundColor:
-                    Colors.grey[200], // Cor de fundo do CircleAvatar
-                child: const Icon(
-                  Icons.account_circle,
-                  size: 144, // Tamanho do ícone dentro do CircleAvatar
-                  color: Colors.grey, // Cor do ícone
-                ),
-              ),
-              const SizedBox(height: 12),
-              Text(
-                fullName ?? 'Loading...',
-                style: const TextStyle(fontSize: 20, color: Colors.white),
-              ),
-            ],
-          ),
-        ),
       ),
     );
   }
