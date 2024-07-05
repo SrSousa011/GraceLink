da6cc083b77c7c669a10a71261ea352f8378651c
Lucas Sousa <seu@email.com>
2024-07-05 10:26:15 +0200

    [ADD] MultiProvider to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:churchapp/provider/user_provider.dart';
 import 'package:churchapp/route/root.dart';
 import 'package:churchapp/route/routes.dart';
 import 'package:flutter/material.dart';
@@ -5,6 +6,7 @@ import 'package:churchapp/services/auth_service.dart';
 import 'package:churchapp/views/nav_bar/nav_bar.dart';
 import 'package:churchapp/views/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -12,7 +14,14 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => UserProvider()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
