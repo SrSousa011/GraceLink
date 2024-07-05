4892d6ba047d9e343629bf43af21af24892e458d
Lucas Sousa <seu@email.com>
2024-07-05 10:27:34 +0200

    [UPDATE] Updauserdata in userprovider

--- lib/provider/user_provider.dart
+++ lib/provider/user_provider.dart
@@ -1,16 +1,48 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import 'package:churchapp/models/user_data.dart';
-import 'package:churchapp/services/auth_method.dart';
-import 'package:flutter/material.dart';
 
 class UserProvider with ChangeNotifier {
   UserData? _user;
-  final AuthMethods _authMethods = AuthMethods();
 
-  UserData? get getUser => _user;
+  UserData? get user => _user;
 
-  Future<void> refreshUser() async {
-    UserData user = await _authMethods.getUserDetails();
-    _user = user;
+  void setUser(UserData userData) {
+    _user = userData;
     notifyListeners();
+  }
+
+  void clearUser() {
+    _user = null;
+    notifyListeners();
+  }
+
+  void updateUserData(Map<String, dynamic> newData) {
+    if (_user != null) {
+      _user = UserData(
+        uid: newData['uid'] ?? _user!.uid,
+        fullName: newData['fullName'] ?? _user!.fullName,
+        email: newData['email'] ?? _user!.email,
+        password: newData['password'] ?? _user!.password,
+        phoneNumber: newData['phoneNumber'] ?? _user!.phoneNumber,
+        address: newData['address'] ?? _user!.address,
+      );
+      notifyListeners();
+    }
+  }
+
+  Future<void> fetchUserData(String uid) async {
+    try {
+      DocumentSnapshot doc =
+          await FirebaseFirestore.instance.collection('users').doc(uid).get();
+      if (doc.exists) {
+        _user = UserData.fromDocumentSnapshot(doc);
+        notifyListeners();
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error fetching user data: $e');
+      }
+    }
   }
 }
