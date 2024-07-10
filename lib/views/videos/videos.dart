14a85e22320a0b8fa458d8934d2831a13c12bcf2
Lucas Sousa <seu@email.com>
2024-07-10 10:15:06 +0200

    [ADD] deletion functionality

--- lib/views/videos/videos.dart
+++ lib/views/videos/videos.dart
@@ -7,7 +7,7 @@ import 'package:churchapp/services/auth_service.dart';
 import 'package:churchapp/views/nav_bar/nav_bar.dart';
 
 class Videos extends StatefulWidget {
-  const Videos({Key? key}) : super(key: key);
+  const Videos({super.key});
 
   @override
   State<Videos> createState() => _VideosState();
@@ -16,6 +16,8 @@ class Videos extends StatefulWidget {
 class _VideosState extends State<Videos> {
   final VideosService _videosService = VideosService();
   final TextEditingController _controller = TextEditingController();
+  bool _isSelectionMode = false;
+  final List<String> _selectedVideos = [];
 
   Future<YT.Video> _fetchVideo(String url) async {
     var yt = YT.YoutubeExplode();
@@ -73,11 +75,82 @@ class _VideosState extends State<Videos> {
     }
   }
 
+  void _toggleSelectionMode() {
+    setState(() {
+      _isSelectionMode = !_isSelectionMode;
+      if (!_isSelectionMode) {
+        _selectedVideos.clear();
+      }
+    });
+  }
+
+  void _toggleVideoSelection(String videoId) {
+    setState(() {
+      if (_selectedVideos.contains(videoId)) {
+        _selectedVideos.remove(videoId);
+      } else {
+        _selectedVideos.add(videoId);
+      }
+    });
+  }
+
+  Future<void> _deleteSelectedVideos(BuildContext context) async {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Confirmar Exclusão'),
+          content: const Text(
+              'Tem certeza que deseja excluir os vídeos selecionados?'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancelar'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Excluir'),
+              onPressed: () async {
+                try {
+                  for (var videoId in _selectedVideos) {
+                    await _videosService.deleteVideo(videoId);
+                  }
+                  setState(() {
+                    _selectedVideos.clear();
+                    _isSelectionMode = false;
+                  });
+                  if (!context.mounted) return;
+                  Navigator.of(context).pop();
+                } catch (e) {
+                  debugPrint('Error deleting videos: $e');
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Erro ao deletar vídeos.'),
+                    ),
+                  );
+                }
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('YouTube Links'),
+        actions: [
+          IconButton(
+            onPressed: _isSelectionMode
+                ? () => _deleteSelectedVideos(context)
+                : _toggleSelectionMode,
+            icon: Icon(_isSelectionMode ? Icons.delete : Icons.list),
+          ),
+        ],
       ),
       drawer: NavBar(
         auth: AuthenticationService(),
@@ -138,59 +211,78 @@ class _VideosState extends State<Videos> {
                     itemCount: sortedList.length,
                     itemBuilder: (context, index) {
                       var videoData = sortedList[index];
+                      var videoId = videoData['id'] as String;
+                      var videoUrl = videoData['url'] as String;
                       return Column(
                         children: [
                           InkWell(
-                            onTap: () => _launchURL(videoData['url']),
+                            onTap: _isSelectionMode
+                                ? () => _toggleVideoSelection(videoId)
+                                : () => _launchURL(videoUrl),
                             child: SizedBox(
                               width: double.infinity,
-                              child: FutureBuilder<YT.Video>(
-                                future: _fetchVideo(videoData['url']),
-                                builder: (context, videoSnapshot) {
-                                  if (videoSnapshot.connectionState ==
-                                      ConnectionState.waiting) {
-                                    return const SizedBox(
-                                      height: 200,
-                                      child: Center(
-                                        child: CircularProgressIndicator(),
-                                      ),
-                                    );
-                                  } else if (videoSnapshot.hasError) {
-                                    return const Icon(Icons.error_outline);
-                                  } else if (videoSnapshot.hasData) {
-                                    var video = videoSnapshot.data!;
-                                    return Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.stretch,
-                                      children: [
-                                        Image.network(
-                                          'https://i.ytimg.com/vi/${video.id}/hqdefault.jpg',
-                                          height:
-                                              180, // Ajusta a altura da imagem para um formato mais retangular
-                                          width: double.infinity,
-                                          fit: BoxFit.cover,
-                                        ),
-                                        const SizedBox(height: 8),
-                                        Padding(
-                                          padding: const EdgeInsets.symmetric(
-                                              horizontal: 8.0),
-                                          child: Text(
-                                            video.title,
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .headline6!
-                                                .copyWith(
-                                                  fontWeight: FontWeight.bold,
-                                                  fontSize: 16,
+                              child: Row(
+                                children: [
+                                  if (_isSelectionMode)
+                                    Checkbox(
+                                      value: _selectedVideos.contains(videoId),
+                                      onChanged: (_) =>
+                                          _toggleVideoSelection(videoId),
+                                    ),
+                                  Expanded(
+                                    child: FutureBuilder<YT.Video>(
+                                      future: _fetchVideo(videoUrl),
+                                      builder: (context, videoSnapshot) {
+                                        if (videoSnapshot.connectionState ==
+                                            ConnectionState.waiting) {
+                                          return const SizedBox(
+                                            height: 200,
+                                            child: Center(
+                                              child:
+                                                  CircularProgressIndicator(),
+                                            ),
+                                          );
+                                        } else if (videoSnapshot.hasError) {
+                                          return const Icon(
+                                              Icons.error_outline);
+                                        } else if (videoSnapshot.hasData) {
+                                          var video = videoSnapshot.data!;
+                                          return Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.stretch,
+                                            children: [
+                                              Image.network(
+                                                'https://i.ytimg.com/vi/${video.id}/hqdefault.jpg',
+                                                height: 180,
+                                                width: double.infinity,
+                                                fit: BoxFit.cover,
+                                              ),
+                                              const SizedBox(height: 8),
+                                              Padding(
+                                                padding:
+                                                    const EdgeInsets.symmetric(
+                                                        horizontal: 8.0),
+                                                child: Text(
+                                                  video.title,
+                                                  style: Theme.of(context)
+                                                      .textTheme
+                                                      .titleLarge!
+                                                      .copyWith(
+                                                        fontWeight:
+                                                            FontWeight.bold,
+                                                        fontSize: 16,
+                                                      ),
                                                 ),
-                                          ),
-                                        ),
-                                      ],
-                                    );
-                                  } else {
-                                    return const SizedBox.shrink();
-                                  }
-                                },
+                                              ),
+                                            ],
+                                          );
+                                        } else {
+                                          return const SizedBox.shrink();
+                                        }
+                                      },
+                                    ),
+                                  ),
+                                ],
                               ),
                             ),
                           ),
