0d3e3c81f414412bb4082ce3fe449108dc990a0c
Lucas Sousa <seu@email.com>
2024-07-19 18:02:12 +0200

    [ADD] toDocumentSnapshot

--- lib/models/user_data.dart
+++ lib/models/user_data.dart
@@ -12,40 +12,32 @@ class UserData {
     required this.address,
     required this.imagePath,
   });
+
   Map<String, dynamic> toJson() {
     return {
       'id': id,
       'fullName': fullName,
       'address': address,
-      'imageUrl': imagePath,
+      'imagePath': imagePath,
     };
   }
 
   factory UserData.fromJson(Map<String, dynamic> json) {
     return UserData(
-      id: json['id'],
-      fullName: json['fullName'],
-      address: json['address'],
-      imagePath: json['imageUrl'],
+      id: json['id'] ?? '',
+      fullName: json['fullName'] ?? '',
+      address: json['address'] ?? '',
+      imagePath: json['imagePath'] ?? '',
     );
   }
 
   factory UserData.fromDocumentSnapshot(DocumentSnapshot doc) {
-    Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+    Map<String, dynamic> data = doc.data() as Map<String, dynamic>? ?? {};
     return UserData(
       id: doc.id,
-      fullName: data['fullName'],
-      address: data['address'],
-      imagePath: data['imageUrl'],
+      fullName: data['fullName'] ?? '',
+      address: data['address'] ?? '',
+      imagePath: data['imagePath'] ?? '',
     );
-  }
-
-  Map<String, dynamic> toDocumentSnapshot() {
-    return {
-      'id': id,
-      'fullName': fullName,
-      'address': address,
-      'imageUrl': imagePath,
-    };
   }
 }
