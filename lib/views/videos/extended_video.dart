c16c7db87e76a2a36c09099f3adc13ed425f70f8
Lucas Sousa <seu@email.com>
2024-07-25 12:38:12 +0200

    [ADD] Extended video

---
+++ lib/views/videos/extended_video.dart
@@ -0,0 +1,8 @@
+import 'package:youtube_explode_dart/youtube_explode_dart.dart' as YT;
+
+class ExtendedVideo {
+  final YT.Video video;
+  final int viewCount;
+
+  ExtendedVideo(this.video, this.viewCount);
+}
