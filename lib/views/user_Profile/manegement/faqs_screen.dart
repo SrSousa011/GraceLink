58b37fe9785778319726e737d4d463f2b290e6f9
Lucas Sousa <seu@email.com>
2024-07-27 20:20:04 +0200

    [ADD] DarkModo

--- lib/views/user_Profile/manegement/faqs_screen.dart
+++ lib/views/user_Profile/manegement/faqs_screen.dart
@@ -1,108 +1,125 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:provider/provider.dart';
+import 'package:churchapp/theme/theme_provider.dart';
 
 class FAQScreen extends StatelessWidget {
   const FAQScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+    final isDarkMode = themeProvider.isDarkMode;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('FAQs'),
+        backgroundColor: isDarkMode ? Colors.blueGrey[900] : Colors.blue,
       ),
       body: Container(
-        color: Colors.black,
+        color: isDarkMode ? Colors.grey[900] : Colors.white,
         child: ListView(
           padding: const EdgeInsets.all(16.0),
           children: [
-            const Text(
+            Text(
               'Perguntas Frequentes',
               style: TextStyle(
                 fontSize: 22,
                 fontWeight: FontWeight.bold,
-                color: Colors.white,
+                color: isDarkMode ? Colors.white : Colors.black,
               ),
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 20),
-            const ExpansionTile(
+            ExpansionTile(
               title: Text(
                 'Qual é o horário dos cultos?',
-                style: TextStyle(color: Colors.white),
+                style:
+                    TextStyle(color: isDarkMode ? Colors.white : Colors.black),
               ),
               children: [
                 Padding(
-                  padding: EdgeInsets.all(16.0),
+                  padding: const EdgeInsets.all(16.0),
                   child: Text(
                     'Os cultos ocorrem aos domingos às 10h e às quartas-feiras às 19h.',
-                    style: TextStyle(color: Colors.white70),
+                    style: TextStyle(
+                        color: isDarkMode ? Colors.white70 : Colors.black54),
                   ),
                 ),
               ],
             ),
-            const ExpansionTile(
+            ExpansionTile(
               title: Text(
                 'Como posso me envolver com os grupos de estudo?',
-                style: TextStyle(color: Colors.white),
+                style:
+                    TextStyle(color: isDarkMode ? Colors.white : Colors.black),
               ),
               children: [
                 Padding(
-                  padding: EdgeInsets.all(16.0),
+                  padding: const EdgeInsets.all(16.0),
                   child: Text(
                     'Você pode se inscrever nos grupos de estudo através do nosso site ou entrando em contato com o escritório da igreja.',
-                    style: TextStyle(color: Colors.white70),
+                    style: TextStyle(
+                        color: isDarkMode ? Colors.white70 : Colors.black54),
                   ),
                 ),
               ],
             ),
-            const ExpansionTile(
+            ExpansionTile(
               title: Text(
                 'Onde posso encontrar informações sobre eventos?',
-                style: TextStyle(color: Colors.white),
+                style:
+                    TextStyle(color: isDarkMode ? Colors.white : Colors.black),
               ),
               children: [
                 Padding(
-                  padding: EdgeInsets.all(16.0),
+                  padding: const EdgeInsets.all(16.0),
                   child: Text(
                     'As informações sobre eventos estão disponíveis no nosso site e no mural de avisos da igreja.',
-                    style: TextStyle(color: Colors.white70),
+                    style: TextStyle(
+                        color: isDarkMode ? Colors.white70 : Colors.black54),
                   ),
                 ),
               ],
             ),
-            const ExpansionTile(
+            ExpansionTile(
               title: Text(
                 'Como posso fazer uma doação?',
-                style: TextStyle(color: Colors.white),
+                style:
+                    TextStyle(color: isDarkMode ? Colors.white : Colors.black),
               ),
               children: [
                 Padding(
-                  padding: EdgeInsets.all(16.0),
+                  padding: const EdgeInsets.all(16.0),
                   child: Text(
                     'Você pode fazer uma doação através do nosso site ou diretamente na igreja durante os horários de culto.',
-                    style: TextStyle(color: Colors.white70),
+                    style: TextStyle(
+                        color: isDarkMode ? Colors.white70 : Colors.black54),
                   ),
                 ),
               ],
             ),
             const SizedBox(height: 20),
-            const Text(
+            Text(
               'Não encontrou a resposta para sua pergunta?',
               style: TextStyle(
                 fontSize: 18,
-                color: Colors.white,
+                color: isDarkMode ? Colors.white : Colors.black,
               ),
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 10),
             ListTile(
-              title: const Text(
+              title: Text(
                 'Entre em contato conosco',
-                style: TextStyle(color: Colors.white),
+                style:
+                    TextStyle(color: isDarkMode ? Colors.white : Colors.black),
               ),
-              leading: const Icon(Icons.contact_mail, color: Colors.white),
+              leading: Icon(
+                Icons.contact_mail,
+                color: isDarkMode ? Colors.blueAccent : Colors.blue,
+              ),
               onTap: () {
-                // Navegar para tela de contato ou abrir email
                 _launchEmail();
               },
             ),
