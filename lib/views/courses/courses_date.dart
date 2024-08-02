5f22bb03cc4cfb3cc4493ff1118e7844d50a66a8
Lucas Sousa <seu@email.com>
2024-08-02 14:29:26 +0200

    [ADD] Courses date

---
+++ lib/views/courses/courses_date.dart
@@ -0,0 +1,53 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Course {
+  final String id;
+  final String title;
+  final String instructor;
+  final String imageURL;
+  final String description;
+  final double price;
+  final DateTime registrationDeadline;
+  final String descriptionDetails;
+
+  Course({
+    required this.id,
+    required this.title,
+    required this.instructor,
+    required this.imageURL,
+    required this.description,
+    required this.price,
+    required this.registrationDeadline,
+    required this.descriptionDetails,
+  });
+
+  factory Course.fromDocument(DocumentSnapshot doc) {
+    final data = doc.data() as Map<String, dynamic>;
+
+    final timestamp = data['registrationDeadline'] as Timestamp?;
+    final registrationDeadline = timestamp?.toDate() ?? DateTime.now();
+
+    return Course(
+      id: doc.id,
+      title: data['title'] ?? '',
+      instructor: data['instructor'] ?? '',
+      imageURL: data['imageURL'] ?? '',
+      description: data['description'] ?? '',
+      price: (data['price'] as num).toDouble(),
+      registrationDeadline: registrationDeadline,
+      descriptionDetails: data['descriptionDetails'] ?? '',
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'instructor': instructor,
+      'imageURL': imageURL,
+      'description': description,
+      'price': price,
+      'registrationDeadline': Timestamp.fromDate(registrationDeadline),
+      'descriptionDetails': descriptionDetails,
+    };
+  }
+}
