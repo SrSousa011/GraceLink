78076adb79fe84f432288f7e3ce3dc44264ff949
Lucas Sousa <seu@email.com>
2024-08-02 17:57:28 +0200

    [ADD] _fetchRegistrations

--- lib/views/courses/adminDashboard/subscribers_list.dart
+++ lib/views/courses/adminDashboard/subscribers_list.dart
@@ -1,12 +1,10 @@
-import 'package:flutter/material.dart';
+import 'package:churchapp/views/courses/adminDashboard/subscriber_viewer.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
-import 'package:churchapp/models/user_data.dart';
+import 'package:flutter/material.dart';
 
 class SubscribersList extends StatefulWidget {
-  final int courseId;
-
-  const SubscribersList({super.key, required this.courseId});
+  const SubscribersList({super.key});
 
   @override
   State<SubscribersList> createState() => _SubscribersListState();
@@ -14,142 +12,90 @@ class SubscribersList extends StatefulWidget {
 
 class _SubscribersListState extends State<SubscribersList> {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  List<DocumentSnapshot> _subscribers = [];
+  List<Map<String, dynamic>> _registrations = [];
   bool _loading = true;
-  String? _courseName;
 
   @override
   void initState() {
     super.initState();
-    _fetchSubscribers();
-    _fetchCourseName(); // Fetch the course name when initializing
+    _fetchRegistrations();
   }
 
-  Future<void> _fetchSubscribers() async {
+  Future<void> _fetchRegistrations() async {
     try {
-      QuerySnapshot snapshot = await _firestore
-          .collection('courseregistration')
-          .where('courseId', isEqualTo: widget.courseId)
-          .get();
+      // Fetch all course registrations
+      QuerySnapshot snapshot =
+          await _firestore.collection('courseregistration').get();
+
       setState(() {
-        _subscribers = snapshot.docs;
+        _registrations = snapshot.docs.map((doc) {
+          final data = doc.data() as Map<String, dynamic>;
+          return {
+            'userId': data['userId'] ?? 'Unknown',
+            'userName': data['userName'] ?? 'Unknown',
+            'registrationDate':
+                (data['registrationDate'] as Timestamp).toDate(),
+            'courseName': data['courseName'] ??
+                'Unknown', // Adjust field name if necessary
+            'status': data['status'] ?? false, // Adjust field name if necessary
+          };
+        }).toList();
         _loading = false;
       });
     } catch (e) {
       if (kDebugMode) {
-        print('Error fetching subscribers: $e');
+        print('Error fetching registrations: $e');
       }
       setState(() {
         _loading = false;
       });
-    }
-  }
-
-  Future<void> _fetchCourseName() async {
-    try {
-      final courseDoc = await _firestore
-          .collection('courses')
-          .doc(widget.courseId.toString())
-          .get();
-      setState(() {
-        _courseName = courseDoc.data()?['name'] ?? 'Unknown Course';
-      });
-    } catch (e) {
-      if (kDebugMode) {
-        print('Error fetching course name: $e');
-      }
-    }
-  }
-
-  Future<UserData> _fetchUserData(String userId) async {
-    try {
-      final userDoc = await _firestore.collection('users').doc(userId).get();
-      return UserData.fromDocument(userDoc);
-    } catch (e) {
-      if (kDebugMode) {
-        print('Error fetching user data: $e');
-      }
-      return UserData(
-        id: userId,
-        fullName: 'Unknown',
-        address: '',
-        imagePath: '',
-        role: 'user',
-      );
     }
   }
 
   @override
   Widget build(BuildContext context) {
     final bool isDarkMode = Theme.of(context).brightness == Brightness.dark;
-
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Subscribers List'),
+        title: const Text('Course Registrations'),
       ),
       body: _loading
           ? const Center(child: CircularProgressIndicator())
           : ListView.builder(
-              itemCount: _subscribers.length,
+              itemCount: _registrations.length,
               itemBuilder: (context, index) {
-                final subscriber =
-                    _subscribers[index].data() as Map<String, dynamic>;
-
-                final userId = subscriber['userId'] ?? '';
-                final status = subscriber['status'] ?? false;
-                final registrationDate =
-                    (subscriber['registrationDate'] as Timestamp?)?.toDate() ??
-                        DateTime.now();
-
-                return FutureBuilder<UserData>(
-                  future: _fetchUserData(userId),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const ListTile(
-                        title: Text('Loading...'),
-                      );
-                    }
-
-                    if (!snapshot.hasData) {
-                      return const ListTile(
-                        title: Text('Error fetching user data'),
-                      );
-                    }
-
-                    final userData = snapshot.data!;
-
-                    return ListTile(
-                      title: Text(userData.fullName),
-                      subtitle: Text(
-                          'Status: ${status ? 'Payed' : 'Not Payed'}\nRegistration Date: ${registrationDate.toLocal()}'),
-                      trailing: ElevatedButton(
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all<Color>(
-                            isDarkMode ? Colors.grey : Colors.blue,
-                          ),
-                          foregroundColor: MaterialStateProperty.all<Color>(
-                            isDarkMode
-                                ? Colors.white
-                                : const Color.fromARGB(255, 255, 255, 255),
+                final registration = _registrations[index];
+                return ListTile(
+                  title: Text(registration['userName']),
+                  subtitle: Text(
+                      'Registered on: ${registration['registrationDate'].toLocal().toString().split(' ')[0]}'),
+                  trailing: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                        isDarkMode ? Colors.grey : Colors.blue,
+                      ),
+                      foregroundColor: MaterialStateProperty.all<Color>(
+                        isDarkMode
+                            ? Colors.white
+                            : const Color.fromARGB(255, 255, 255, 255),
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SubscriberViewer(
+                            userId: registration['userId'],
+                            userName: registration['userName'],
+                            status: registration['status'],
+                            registrationDate: registration['registrationDate'],
+                            courseName: registration['courseName'],
                           ),
                         ),
-                        onPressed: () {
-                          Navigator.pushNamed(
-                            context,
-                            '/subscriber_viewer',
-                            arguments: {
-                              'userId': userData.id,
-                              'userName': userData.fullName,
-                              'status': status,
-                              'registrationDate': registrationDate,
-                              'courseName': _courseName ?? 'Unknown Course',
-                            },
-                          );
-                        },
-                        child: const Text('View Details'),
-                      ),
-                    );
-                  },
+                      );
+                    },
+                    child: const Text('View Details'),
+                  ),
                 );
               },
             ),
