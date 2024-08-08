6341ccd190452b65f44d81fcea8dba025199f58c
Lucas Sousa <seu@email.com>
2024-08-08 10:14:29 +0200

    [ADD] Event details

---
+++ lib/views/events/event_detail/event_details.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class EventDetails extends StatelessWidget {
+  final String title;
+  final String description;
+  final String date;
+  final String time;
+  final String location;
+  final bool isDarkMode;
+
+  const EventDetails({
+    super.key,
+    required this.title,
+    required this.description,
+    required this.date,
+    required this.time,
+    required this.location,
+    required this.isDarkMode,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Title: $title',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: isDarkMode ? Colors.white : Colors.black,
+          ),
+        ),
+        _buildDetailsText('Description: $description'),
+        _buildDetailsText('Date: $date'),
+        _buildDetailsText('Time: $time'),
+        _buildDetailsText('Location: $location'),
+        const SizedBox(height: 16.0),
+      ],
+    );
+  }
+
+  Widget _buildDetailsText(String text) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Text(
+        text,
+        style: TextStyle(
+          fontSize: 16,
+          color: isDarkMode ? Colors.white : Colors.black,
+        ),
+      ),
+    );
+  }
+}
