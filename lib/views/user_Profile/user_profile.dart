0c53bac803afb0aeb0d2023c134cf9e48c495011
Lucas Sousa <seu@email.com>
2024-08-13 18:27:26 +0200

    [UPDATE] ProfileScreen: Integrate real-time user data and profile picture update

--- lib/views/user_Profile/user_profile.dart
+++ lib/views/user_Profile/user_profile.dart
@@ -32,15 +32,11 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   late ImagePickerr _imagePicker;
   late User _user;
-  late UserData _userData;
-  String? _imageUrl;
 
   @override
   void initState() {
     super.initState();
     _user = FirebaseAuth.instance.currentUser!;
-    _userData = widget.userData;
-    _imageUrl = _userData.imagePath;
 
     _imagePicker = ImagePickerr(
       storage: FirebaseStorage.instance,
@@ -50,12 +46,11 @@ class _ProfileScreenState extends State<ProfileScreen> {
     );
   }
 
-  void _updateProfilePicture() {
-    _imagePicker.updateProfilePicture(
+  void _updateProfilePicture() async {
+    await _imagePicker.updateProfilePicture(
       () {
-        setState(() {
-          _imageUrl = _userData.imagePath;
-        });
+        // Atualize o estado após o upload da imagem
+        setState(() {});
       },
       (error) {
         if (kDebugMode) {
@@ -94,133 +89,155 @@ class _ProfileScreenState extends State<ProfileScreen> {
             ),
           ],
         ),
-        body: SingleChildScrollView(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Stack(
+        body: StreamBuilder<DocumentSnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection('users')
+              .doc(_user.uid)
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
+
+            if (!snapshot.hasData || !snapshot.data!.exists) {
+              return const Center(child: Text('User data not found'));
+            }
+
+            UserData userData = UserData.fromDocument(snapshot.data!);
+
+            return SingleChildScrollView(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    GestureDetector(
-                      onTap: _updateProfilePicture,
-                      child: CircleAvatar(
-                        radius: 72,
-                        backgroundColor:
-                            isDarkMode ? Colors.grey[800] : Colors.grey[200],
-                        backgroundImage: _imageUrl != null
-                            ? CachedNetworkImageProvider(_imageUrl!)
-                            : const AssetImage(tAvatar) as ImageProvider,
+                    Stack(
+                      children: [
+                        GestureDetector(
+                          onTap: _updateProfilePicture,
+                          child: CircleAvatar(
+                            radius: 72,
+                            backgroundColor: isDarkMode
+                                ? Colors.grey[800]
+                                : Colors.grey[200],
+                            backgroundImage: userData.imagePath.isNotEmpty
+                                ? CachedNetworkImageProvider(userData.imagePath)
+                                : const AssetImage(tAvatar) as ImageProvider,
+                          ),
+                        ),
+                        Positioned(
+                          top: 0,
+                          right: 0,
+                          child: Container(
+                            width: 35,
+                            height: 35,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color:
+                                  isDarkMode ? Colors.grey[600] : Colors.blue,
+                            ),
+                            child: IconButton(
+                              icon: const Icon(
+                                Icons.camera_alt,
+                                color: Colors.white,
+                                size: 20,
+                              ),
+                              onPressed: _updateProfilePicture,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 16.0),
+                    Text(
+                      userData.fullName,
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: userNameColor),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 8.0),
+                    Text(
+                      userData.address,
+                      style: TextStyle(
+                          fontSize: 16,
+                          color:
+                              isDarkMode ? Colors.white70 : Colors.grey[700]),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 20),
+                    SizedBox(
+                      width: 200,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: isDarkMode
+                              ? const Color(0xFF333333)
+                              : Colors.blue,
+                          shape: const StadiumBorder(),
+                          foregroundColor: Colors.white,
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  UpdateProfileScreen(userData: userData),
+                            ),
+                          );
+                        },
+                        child: const Text(
+                          'Edit Profile',
+                        ),
                       ),
                     ),
-                    Positioned(
-                      top: 0,
-                      right: 0,
-                      child: Container(
-                        width: 35,
-                        height: 35,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: isDarkMode ? Colors.grey[600] : Colors.blue,
-                        ),
-                        child: IconButton(
-                          icon: const Icon(
-                            Icons.camera_alt,
-                            color: Colors.white,
-                            size: 20,
+                    const SizedBox(height: 30),
+                    const Divider(),
+                    const SizedBox(height: 10),
+                    ProfileMenuWidget(
+                      title: 'Settings',
+                      icon: LineAwesomeIcons.cog_solid,
+                      iconColor: iconColor,
+                      onPress: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const SettingsScreen(),
                           ),
-                          onPressed: _updateProfilePicture,
-                        ),
-                      ),
+                        );
+                      },
+                    ),
+                    ProfileMenuWidget(
+                      title: 'User Management',
+                      icon: LineAwesomeIcons.user_check_solid,
+                      iconColor: iconColor,
+                      onPress: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const UserManagementScreen(),
+                          ),
+                        );
+                      },
+                    ),
+                    ProfileMenuWidget(
+                      title: 'Info',
+                      icon: LineAwesomeIcons.info_solid,
+                      iconColor: iconColor,
+                      onPress: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const InfoScreen(),
+                          ),
+                        );
+                      },
                     ),
                   ],
                 ),
-                const SizedBox(height: 16.0),
-                Text(
-                  _userData.fullName,
-                  style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: userNameColor),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(height: 8.0),
-                Text(
-                  _userData.address,
-                  style: TextStyle(
-                      fontSize: 16,
-                      color: isDarkMode ? Colors.white70 : Colors.grey[700]),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(height: 20),
-                SizedBox(
-                  width: 200,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor:
-                          isDarkMode ? const Color(0xFF333333) : Colors.blue,
-                      shape: const StadiumBorder(),
-                      foregroundColor: Colors.white,
-                    ),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) =>
-                              UpdateProfileScreen(userData: widget.userData),
-                        ),
-                      );
-                    },
-                    child: const Text(
-                      'Edit Profile',
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 30),
-                const Divider(),
-                const SizedBox(height: 10),
-                ProfileMenuWidget(
-                  title: 'Settings',
-                  icon: LineAwesomeIcons.cog_solid,
-                  iconColor: iconColor,
-                  onPress: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const SettingsScreen(),
-                      ),
-                    );
-                  },
-                ),
-                ProfileMenuWidget(
-                  title: 'User Management',
-                  icon: LineAwesomeIcons.user_check_solid,
-                  iconColor: iconColor,
-                  onPress: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const UserManagementScreen(),
-                      ),
-                    );
-                  },
-                ),
-                ProfileMenuWidget(
-                  title: 'Info',
-                  icon: LineAwesomeIcons.info_solid,
-                  iconColor: iconColor,
-                  onPress: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const InfoScreen(),
-                      ),
-                    );
-                  },
-                ),
-              ],
-            ),
-          ),
+              ),
+            );
+          },
         ),
         drawer: NavBar(
           auth: AuthenticationService(),
