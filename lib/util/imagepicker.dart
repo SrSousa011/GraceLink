5bbd4ef901dc7b167eac798bc8362258db23c383
Lucas Sousa <seu@email.com>
2024-08-13 18:20:41 +0200

    [ADD] ImagePicker

--- lib/util/imagepicker.dart
+++ lib/util/imagepicker.dart
@@ -1,14 +1,53 @@
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 
+import 'package:flutter/foundation.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class ImagePickerr {
-  Future<File> uploadImage(String inputSource) async {
-    final picker = ImagePicker();
-    final XFile? pickerImage = await picker.pickImage(
-      source:
-          inputSource == 'cammera' ? ImageSource.camera : ImageSource.gallery,
-    );
-    File imageFile = File(pickerImage!.path);
-    return imageFile;
+  final FirebaseStorage _storage;
+  final FirebaseFirestore _firestore;
+  final ImagePicker _picker;
+  final String userId;
+
+  ImagePickerr({
+    required FirebaseStorage storage,
+    required FirebaseFirestore firestore,
+    required ImagePicker picker,
+    required this.userId,
+  })  : _storage = storage,
+        _firestore = firestore,
+        _picker = picker;
+
+  Future<void> updateProfilePicture(
+      VoidCallback onSuccess, Function(String)? onError) async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      File file = File(pickedFile.path);
+      try {
+        UploadTask uploadTask = _storage
+            .ref()
+            .child('userProfilePictures/$userId/profilePicture.jpg')
+            .putFile(file);
+
+        TaskSnapshot taskSnapshot = await uploadTask;
+        String downloadURL = await taskSnapshot.ref.getDownloadURL();
+
+        await _firestore
+            .collection('users')
+            .doc(userId)
+            .update({'imagePath': downloadURL});
+
+        onSuccess();
+      } catch (e) {
+        if (kDebugMode) {
+          print("Error uploading image: $e");
+        }
+        if (onError != null) {
+          onError(e.toString());
+        }
+      }
+    }
   }
 }
