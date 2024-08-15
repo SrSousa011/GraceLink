545b91f2a0fd5e5875a40fe2260cfad88b3d7265
Lucas Sousa <seu@email.com>
2024-08-15 12:32:06 +0200

    [ADD] CourseSelectionDropdown

---
+++ lib/views/materials/course_selection.dart
@@ -0,0 +1,27 @@
+import 'package:churchapp/views/courses/courses_date.dart';
+import 'package:flutter/material.dart';
+
+class CourseSelectionDropdown extends StatelessWidget {
+  final List<Course> courses;
+  final ValueChanged<Course?> onChanged;
+
+  const CourseSelectionDropdown({
+    super.key,
+    required this.courses,
+    required this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<Course>(
+      hint: const Text('Select a Course'),
+      items: courses.map((course) {
+        return DropdownMenuItem<Course>(
+          value: course,
+          child: Text(course.title),
+        );
+      }).toList(),
+      onChanged: onChanged,
+    );
+  }
+}
