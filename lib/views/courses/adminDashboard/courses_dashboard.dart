92b3a5d3adfc0cdc4313e880919733f83ca815bc
Lucas Sousa <seu@email.com>
2024-08-27 17:50:05 +0200

    [ADD] CourseLive

--- lib/views/courses/adminDashboard/courses_dashboard.dart
+++ lib/views/courses/adminDashboard/courses_dashboard.dart
@@ -1,3 +1,4 @@
+import 'package:churchapp/views/courses/adminDashboard/course_live.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -173,6 +174,20 @@ class CoursesDashboard extends StatelessWidget {
                     ));
                   },
                   child: const Text('Lista de Cadastrados'),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: buttonBackgroundColor,
+                    shape: const StadiumBorder(),
+                    foregroundColor: Colors.white,
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const CourseLive(),
+                    ));
+                  },
+                  child: const Text('Acesso à Videoaula'),
                 ),
               ],
             );
