8d043fa23d7819b386b345b3d124d89a47ccc27d
Lucas Sousa <seu@email.com>
2024-08-27 18:06:30 +0200

    [ADD] courseLive

--- lib/views/courses/adminDashboard/courses_dashboard.dart
+++ lib/views/courses/adminDashboard/courses_dashboard.dart
@@ -1,4 +1,4 @@
-import 'package:churchapp/views/courses/adminDashboard/course_live.dart';
+import 'package:churchapp/views/courses/courseLive/course_live.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
