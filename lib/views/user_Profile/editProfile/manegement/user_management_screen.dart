828359c700d9bba36887dd65fdf3895733b8eaeb
Lucas Sousa <seu@email.com>
2024-08-28 17:06:16 +0200

    [UPDATE] Widgets that you can use again

--- lib/views/user_Profile/editProfile/manegement/user_management_screen.dart
+++ lib/views/user_Profile/editProfile/manegement/user_management_screen.dart
@@ -15,64 +15,77 @@ class UserManagementScreen extends StatelessWidget {
       body: ListView(
         padding: const EdgeInsets.all(16.0),
         children: [
-          ListTile(
-            title: const Text('Desativar Usuário',
-                style: TextStyle(color: Colors.orange)),
-            leading: const Icon(Icons.remove_circle, color: Colors.orange),
-            onTap: () {
-              _showConfirmationDialog(
-                context,
-                'Desativar Usuário',
-                'Tem certeza de que deseja desativar este usuário?',
-                () async {
-                  await _authService.desactivateUser();
-                  if (!context.mounted) return;
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                        content: Text('Usuário desativado com sucesso.')),
-                  );
-                },
-              );
-            },
-          ),
-          ListTile(
-            title: const Text('Excluir Usuário',
-                style: TextStyle(color: Colors.red)),
-            leading: const Icon(Icons.delete, color: Colors.red),
-            onTap: () {
-              _showConfirmationDialog(
-                context,
-                'Excluir Usuário',
-                'Tem certeza de que deseja excluir este usuário?',
-                () async {
-                  await _authService.deleteUser();
-                  if (!context.mounted) return;
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                        content: Text('Usuário excluído com sucesso.')),
-                  );
-                },
-              );
-            },
-          ),
-          ListTile(
-            title: const Text('Enviar Confirmação por E-mail',
-                style: TextStyle(color: Colors.blue)),
-            leading: const Icon(Icons.email, color: Colors.blue),
-            onTap: () {
-              _showEmailDialog(context);
-            },
-          ),
-          ListTile(
-            title: const Text('Enviar Código de Verificação por Telefone',
-                style: TextStyle(color: Colors.blue)),
-            leading: const Icon(Icons.phone, color: Colors.blue),
-            onTap: () {
-              _showPhoneVerificationDialog(context);
-            },
-          ),
+          _buildEmailConfirmationTile(context),
+          _buildPhoneVerificationTile(context),
+          _buildDeactivateUserTile(context),
+          _buildDeleteUserTile(context),
         ],
       ),
+    );
+  }
+
+  Widget _buildDeactivateUserTile(BuildContext context) {
+    return ListTile(
+      title: const Text('Desativar Usuário',
+          style: TextStyle(color: Colors.orange)),
+      leading: const Icon(Icons.remove_circle, color: Colors.orange),
+      onTap: () {
+        _showConfirmationDialog(
+          context,
+          'Desativar Usuário',
+          'Tem certeza de que deseja desativar este usuário?',
+          () async {
+            await _authService.desactivateUser();
+            if (!context.mounted) return;
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Usuário desativado com sucesso.')),
+            );
+          },
+        );
+      },
+    );
+  }
+
+  Widget _buildDeleteUserTile(BuildContext context) {
+    return ListTile(
+      title: const Text('Excluir Usuário', style: TextStyle(color: Colors.red)),
+      leading: const Icon(Icons.delete, color: Colors.red),
+      onTap: () {
+        _showConfirmationDialog(
+          context,
+          'Excluir Usuário',
+          'Tem certeza de que deseja excluir este usuário?',
+          () async {
+            await _authService.deleteUser();
+            if (!context.mounted) return;
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Usuário excluído com sucesso.')),
+            );
+          },
+        );
+      },
+    );
+  }
+
+  Widget _buildEmailConfirmationTile(BuildContext context) {
+    return ListTile(
+      title:
+          const Text('Confirmar E-mail', style: TextStyle(color: Colors.black)),
+      leading: const Icon(Icons.email, color: Colors.blue),
+      onTap: () {
+        _showEmailDialog(context);
+      },
+    );
+  }
+
+  Widget _buildPhoneVerificationTile(BuildContext context) {
+    return ListTile(
+      title: const Text('Confirmar Telefone',
+          style: TextStyle(color: Colors.black)),
+      leading: const Icon(Icons.phone, color: Colors.blue),
+      onTap: () {
+        _showPhoneVerificationDialog(context);
+      },
     );
   }
 
@@ -132,6 +145,7 @@ class UserManagementScreen extends StatelessWidget {
               onPressed: () async {
                 Navigator.of(context).pop();
                 await _authService.sendEmailConfirmation(emailController.text);
+                if (!context.mounted) return;
                 ScaffoldMessenger.of(context).showSnackBar(
                   const SnackBar(
                       content: Text('Confirmação por e-mail enviada.')),
@@ -151,7 +165,7 @@ class UserManagementScreen extends StatelessWidget {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: const Text('Enviar Código de Verificação por Telefone'),
+          title: const Text('Confirmar Telefone'),
           content: TextField(
             controller: phoneController,
             decoration: const InputDecoration(labelText: 'Número de Telefone'),
@@ -168,6 +182,7 @@ class UserManagementScreen extends StatelessWidget {
               onPressed: () async {
                 Navigator.of(context).pop();
                 await _authService.sendVerificationCode(phoneController.text);
+                if (!context.mounted) return;
                 ScaffoldMessenger.of(context).showSnackBar(
                   const SnackBar(
                       content: Text('Código de verificação enviado.')),
