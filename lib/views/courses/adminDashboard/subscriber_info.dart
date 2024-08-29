ae7e8056d70b7ec0b6976c87a04ebf138ef4f79a
Lucas Sousa <seu@email.com>
2024-08-29 18:08:42 +0200

    [UPDATE] Refactor SubscriberInfo UI and functionality

--- lib/views/courses/adminDashboard/subscriber_info.dart
+++ lib/views/courses/adminDashboard/subscriber_info.dart
@@ -1,8 +1,10 @@
-import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class SubscriberInfo extends StatefulWidget {
   final String userId;
+  final bool status;
   final String userName;
   final DateTime registrationDate;
   final String courseName;
@@ -11,6 +13,7 @@ class SubscriberInfo extends StatefulWidget {
   const SubscriberInfo({
     super.key,
     required this.userId,
+    required this.status,
     required this.userName,
     required this.registrationDate,
     required this.courseName,
@@ -22,118 +25,46 @@ class SubscriberInfo extends StatefulWidget {
 }
 
 class _SubscriberInfoState extends State<SubscriberInfo> {
-  bool? _status;
-  late DocumentReference<Map<String, dynamic>> _docRef;
-  late Stream<DocumentSnapshot<Map<String, dynamic>>> _statusStream;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  @override
-  void initState() {
-    super.initState();
-
-    _docRef = FirebaseFirestore.instance
-        .collection('courseRegistration')
-        .doc(widget.userId);
-
-    _statusStream = _docRef.snapshots();
-
-    _statusStream.listen((docSnapshot) {
-      if (docSnapshot.exists) {
-        final data = docSnapshot.data();
-        if (data != null) {
-          setState(() {
-            _status = data['status'] ?? false;
-          });
-        } else {
-          setState(() {
-            _status = false;
-          });
-        }
-      } else {
-        setState(() {
-          _status = false;
-        });
-      }
-    });
-  }
-
-  Future<void> _toggleStatus() async {
-    if (_status == null) return;
-
+  Future<void> _updateStatus(bool newStatus) async {
     try {
-      final newStatus = !_status!;
-      await _docRef.update({'status': newStatus});
-      setState(() {
-        _status = newStatus;
-      });
+      await _firestore
+          .collection('courseRegistration')
+          .doc(widget.userId)
+          .update({'status': newStatus});
+      setState(() {});
     } catch (e) {
+      if (!mounted) return;
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('Error updating status: $e'),
-        ),
+        SnackBar(content: Text('Erro ao atualizar status: $e')),
       );
     }
   }
 
-  Color _getBackgroundColor(BuildContext context) {
-    return Theme.of(context).brightness == Brightness.dark
-        ? Colors.grey[900]!
-        : Colors.white;
-  }
-
-  Color _getAppBarColor(BuildContext context) {
-    return Theme.of(context).brightness == Brightness.dark
-        ? Colors.grey[850]!
-        : const Color.fromARGB(255, 255, 255, 255);
-  }
-
-  Color _getCardColor(BuildContext context) {
-    return Theme.of(context).brightness == Brightness.dark
-        ? Colors.grey[800]!
-        : Colors.grey[100]!;
-  }
-
-  Color _getTextColor(BuildContext context) {
-    return Theme.of(context).brightness == Brightness.dark
-        ? Colors.white
-        : Colors.black;
-  }
-
-  Color _getSubtitleColor(BuildContext context) {
-    return Theme.of(context).brightness == Brightness.dark
-        ? Colors.grey[300]!
-        : Colors.grey[600]!;
-  }
-
-  Color _getStatusColor() {
-    if (_status == null) return Colors.grey;
-    return _status! ? Colors.green[700]! : Colors.red[700]!;
-  }
-
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final primaryColor =
-        theme.brightness == Brightness.dark ? Colors.blueGrey : Colors.blue;
-
-    String formattedDate =
-        '${widget.registrationDate.day.toString().padLeft(2, '0')}/'
-        '${widget.registrationDate.month.toString().padLeft(2, '0')}/'
-        '${widget.registrationDate.year}';
+    final isDarkMode = Theme.of(context).brightness == Brightness.dark;
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Detalhes do Assinante'),
-        backgroundColor: _getAppBarColor(context),
+        title: const Text('Informações do Assinante'),
+        backgroundColor: isDarkMode
+            ? Colors.grey[850]
+            : const Color.fromARGB(255, 255, 255, 255),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back,
+              color: isDarkMode ? Colors.white : Colors.black),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
       ),
-      backgroundColor: _getBackgroundColor(context),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Card(
-              color: _getCardColor(context),
               elevation: 4.0,
+              color: isDarkMode ? Colors.grey[800] : Colors.grey[100],
               child: Padding(
                 padding: const EdgeInsets.all(16.0),
                 child: Column(
@@ -142,105 +73,112 @@ class _SubscriberInfoState extends State<SubscriberInfo> {
                     Row(
                       children: [
                         CircleAvatar(
+                          radius: 40,
                           backgroundImage: widget.imagePath.isNotEmpty
                               ? NetworkImage(widget.imagePath)
                               : null,
-                          radius: 30,
+                          backgroundColor:
+                              isDarkMode ? Colors.grey[700] : Colors.grey[300],
                           child: widget.imagePath.isEmpty
-                              ? Icon(Icons.person,
-                                  color: _getTextColor(context))
+                              ? Icon(
+                                  Icons.person,
+                                  color:
+                                      isDarkMode ? Colors.white : Colors.black,
+                                )
                               : null,
                         ),
-                        const SizedBox(width: 16.0),
+                        const SizedBox(width: 16),
                         Expanded(
-                          child: Text(
-                            widget.userName,
-                            style: theme.textTheme.titleLarge?.copyWith(
-                                fontWeight: FontWeight.bold,
-                                color: _getTextColor(context)),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                widget.userName,
+                                style: TextStyle(
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold,
+                                  color:
+                                      isDarkMode ? Colors.white : Colors.black,
+                                ),
+                              ),
+                              const SizedBox(height: 8),
+                              Text(
+                                'Curso: ${widget.courseName}',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: isDarkMode
+                                      ? Colors.grey[300]
+                                      : Colors.black54,
+                                ),
+                              ),
+                              const SizedBox(height: 8),
+                              Text(
+                                'Data de Inscrição: ${DateFormat('d MMMM yyyy').format(widget.registrationDate)}',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: isDarkMode
+                                      ? Colors.grey[300]
+                                      : Colors.black54,
+                                ),
+                              ),
+                              const SizedBox(height: 16),
+                              Text(
+                                'Status:',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color:
+                                      isDarkMode ? Colors.white : Colors.black,
+                                ),
+                              ),
+                              const SizedBox(height: 8),
+                              Container(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 8.0, horizontal: 16.0),
+                                decoration: BoxDecoration(
+                                  color: widget.status
+                                      ? Colors.green[700]
+                                      : Colors.red[700],
+                                  borderRadius: BorderRadius.circular(12.0),
+                                ),
+                                child: Text(
+                                  widget.status ? 'Pago' : 'Não Pago',
+                                  style: const TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                       ],
                     ),
-                    const SizedBox(height: 8.0),
-                    Text(
-                      'Curso: ${widget.courseName}',
-                      style: theme.textTheme.titleMedium
-                          ?.copyWith(color: _getSubtitleColor(context)),
-                    ),
-                    const SizedBox(height: 8.0),
-                    Text(
-                      'Data de Inscrição: $formattedDate',
-                      style: theme.textTheme.titleMedium
-                          ?.copyWith(color: _getSubtitleColor(context)),
-                    ),
-                    const SizedBox(height: 16.0),
-                    Text(
-                      'Status:',
-                      style: theme.textTheme.titleMedium?.copyWith(
-                          fontWeight: FontWeight.bold,
-                          color: _getTextColor(context)),
-                    ),
-                    const SizedBox(height: 8.0),
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 8.0, horizontal: 16.0),
-                      decoration: BoxDecoration(
-                        color: _getStatusColor(),
-                        borderRadius: BorderRadius.circular(12.0),
-                      ),
-                      child: Text(
-                        _status == null
-                            ? 'Carregando...'
-                            : (_status! ? 'Pago' : 'Não Pago'),
-                        style: const TextStyle(
-                            color: Colors.white, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    const SizedBox(height: 16.0),
                   ],
                 ),
               ),
             ),
-            const SizedBox(height: 16.0),
+            const SizedBox(height: 24),
             Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                SizedBox(
-                  width: 150.0,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: primaryColor,
-                      padding: const EdgeInsets.symmetric(vertical: 12.0),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12.0),
-                      ),
-                    ),
-                    child: const Text('Voltar',
-                        style: TextStyle(color: Colors.white)),
+                ElevatedButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: isDarkMode ? Colors.blueGrey : Colors.blue,
+                  ),
+                  child: const Text(
+                    'Voltar',
+                    style: TextStyle(color: Colors.white),
                   ),
                 ),
-                const SizedBox(width: 16.0),
-                SizedBox(
-                  width: 150.0,
-                  child: ElevatedButton(
-                    onPressed: _toggleStatus,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: primaryColor,
-                      padding: const EdgeInsets.symmetric(vertical: 12.0),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12.0),
-                      ),
-                    ),
-                    child: Text(
-                      _status == null
-                          ? 'Carregando...'
-                          : (_status! ? 'Não Pago' : 'Pago'),
-                      style: const TextStyle(color: Colors.white),
-                    ),
+                ElevatedButton(
+                  onPressed: () => _updateStatus(!widget.status),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: widget.status ? Colors.red : Colors.green,
+                  ),
+                  child: Text(
+                    widget.status ? 'Não Pago' : 'Pago',
+                    style: const TextStyle(color: Colors.white),
                   ),
                 ),
               ],
