d9481e6a9ca88bd935d1ea017fe25eb5807fb00c
Lucas Sousa <seu@email.com>
2024-09-02 21:34:42 +0200

    [DELETTE] Removing comments

--- lib/views/events/events.dart
+++ lib/views/events/events.dart
@@ -187,8 +187,8 @@ class _EventsState extends State<Events> {
     return PageRouteBuilder(
       pageBuilder: (context, animation, secondaryAnimation) => page,
       transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        const begin = Offset(1.0, 0.0); // Start from right
-        const end = Offset.zero; // End at the center
+        const begin = Offset(1.0, 0.0);
+        const end = Offset.zero;
         const curve = Curves.easeInOut;
 
         var tween = Tween(begin: begin, end: end);
