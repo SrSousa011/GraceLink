85bc5bae536e929c257990de966759fb3a54959c
Lucas Sousa <seu@email.com>
2024-09-16 08:51:10 +0200

    [ADD] PermissionReq

--- lib/views/photos/ask_permission.dart
+++ lib/views/photos/ask_permission.dart
@@ -1,87 +1,143 @@
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'dart:io' show Platform;
 
-class PermissionHandler {
-  static Future<void> requestStoragePermission(BuildContext context) async {
-    final status = await Permission.storage.status;
-    if (!status.isGranted) {
-      final result = await Permission.storage.request();
-      if (!result.isGranted) {
-        if (!context.mounted) return;
-        _showPermissionDialog(context);
+class PermissionReq extends StatefulWidget {
+  const PermissionReq({super.key});
+
+  @override
+  State<PermissionReq> createState() => _PermissionReqState();
+}
+
+Future<void> requestAllPermissionsAndroid() async {
+  // Solicite permissões básicas
+  await Permission.camera.request();
+  await Permission.storage.request();
+
+  // Solicite permissão de gerenciamento de armazenamento se necessário
+  if (await Permission.camera.isDenied || await Permission.storage.isDenied) {
+    await Permission.manageExternalStorage.request();
+  }
+}
+
+Future<void> requestAllPermissionsIOS() async {
+  // Solicite permissões de câmera e fotos
+  await Permission.camera.request();
+  await Permission.photos.request();
+}
+
+class _PermissionReqState extends State<PermissionReq> {
+  bool _isPermissionGranted = false;
+  String _permissionStatus = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _checkAndRequestPermissions();
+  }
+
+  Future<void> _checkAndRequestPermissions() async {
+    bool granted;
+    try {
+      if (Platform.isAndroid) {
+        await requestAllPermissionsAndroid();
+        granted = await _areAllPermissionsGrantedAndroid();
+      } else if (Platform.isIOS) {
+        await requestAllPermissionsIOS();
+        granted = await _areAllPermissionsGrantedIOS();
+      } else {
+        granted = false;
       }
+
+      setState(() {
+        _isPermissionGranted = granted;
+        _permissionStatus =
+            granted ? 'Permissions granted!' : 'Permissions denied.';
+      });
+    } catch (e) {
+      setState(() {
+        _isPermissionGranted = false;
+        _permissionStatus = 'Failed to check permissions: ${e.toString()}';
+      });
     }
   }
 
-  static Future<void> _showPermissionDialog(BuildContext context) async {
-    await showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15.0),
-          ),
-          title: const Row(
-            children: [
-              Icon(Icons.storage, color: Colors.blue),
-              SizedBox(width: 8),
-              Text('Permissão Necessária'),
-            ],
-          ),
-          content: const Text(
-            'Precisamos de sua permissão para acessar o armazenamento e salvar fotos. Isso é necessário para que o aplicativo funcione corretamente.',
-            style: TextStyle(fontSize: 16),
-          ),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () async {
-                final result = await Permission.storage.request();
-                if (result.isGranted) {
-                  if (!context.mounted) return;
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                        content: Text('Permissão concedida com sucesso!')),
-                  );
-                } else {
-                  if (!context.mounted) return;
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                        content: Text(
-                            'Permissão necessária para salvar imagens. Por favor, conceda a permissão nas configurações.')),
-                  );
-                }
-              },
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.white,
-                backgroundColor: Colors.blue,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
-                ),
-              ),
-              child: const Text('Permitir'),
+  Future<void> _ensurePermissions() async {
+    bool granted = false;
+    try {
+      while (!granted) {
+        if (Platform.isAndroid) {
+          await requestAllPermissionsAndroid();
+          granted = await _areAllPermissionsGrantedAndroid();
+        } else if (Platform.isIOS) {
+          await requestAllPermissionsIOS();
+          granted = await _areAllPermissionsGrantedIOS();
+        } else {
+          granted = false;
+        }
+
+        if (granted) {
+          setState(() {
+            _isPermissionGranted = true;
+            _permissionStatus = 'Permissions granted!';
+          });
+          return;
+        } else {
+          setState(() {
+            _isPermissionGranted = false;
+            _permissionStatus = 'Permissions denied. Please try again.';
+          });
+          await Future.delayed(
+              const Duration(seconds: 2)); // Delay before retry
+        }
+      }
+    } catch (e) {
+      setState(() {
+        _isPermissionGranted = false;
+        _permissionStatus = 'Failed to ensure permissions: ${e.toString()}';
+      });
+    }
+  }
+
+  Future<bool> _areAllPermissionsGrantedAndroid() async {
+    return (await Permission.camera.isGranted &&
+        await Permission.storage.isGranted &&
+        (await Permission.manageExternalStorage.status).isGranted);
+  }
+
+  Future<bool> _areAllPermissionsGrantedIOS() async {
+    return (await Permission.camera.isGranted &&
+        await Permission.photos.isGranted);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Storage Permission')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: _checkAndRequestPermissions,
+              child: const Text('Check Permissions'),
             ),
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(
-                    content: Text('Funcionalidade limitada sem permissão.'),
-                  ),
-                );
-              },
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.white,
-                backgroundColor: Colors.grey,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
-                ),
+            const SizedBox(height: 20),
+            Text(
+              _permissionStatus,
+              style: TextStyle(
+                fontSize: 16,
+                color: _isPermissionGranted ? Colors.green : Colors.red,
               ),
-              child: const Text('Cancelar'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _ensurePermissions,
+              child: const Text('Ensure Permissions'),
             ),
           ],
-        );
-      },
+        ),
+      ),
     );
   }
 }
