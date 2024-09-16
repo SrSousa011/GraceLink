273f46cb2697cba487653bc3d293ba71dafb1067
Lucas Sousa <seu@email.com>
2024-09-16 09:33:58 +0200

    [DELETE] Popscone no longer used

--- lib/views/courses/courses/courses.dart
+++ lib/views/courses/courses/courses.dart
@@ -51,150 +51,146 @@ class _CoursesState extends State<Courses> {
     final descriptionColor = isDarkMode ? Colors.white70 : Colors.black87;
     final priceColor = isDarkMode ? Colors.white70 : Colors.black54;
 
-    return PopScope(
-      canPop: false,
-      onPopInvoked: (didPop) {},
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Cursos'),
-          centerTitle: true,
-        ),
-        drawer: const NavBar(),
-        body: FutureBuilder<List<Course>>(
-          future: _coursesFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            }
-            if (snapshot.hasError) {
-              return Center(child: Text('Error: ${snapshot.error}'));
-            }
-            if (!snapshot.hasData || snapshot.data!.isEmpty) {
-              return const Center(child: Text('No courses available.'));
-            }
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cursos'),
+        centerTitle: true,
+      ),
+      drawer: const NavBar(),
+      body: FutureBuilder<List<Course>>(
+        future: _coursesFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+          if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No courses available.'));
+          }
 
-            final coursesList = snapshot.data!;
+          final coursesList = snapshot.data!;
 
-            return ListView.builder(
-              itemCount: coursesList.length,
-              itemBuilder: (context, index) {
-                final course = coursesList[index];
-                return Padding(
-                  padding: const EdgeInsets.only(bottom: 16.0),
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => CoursesDetails(
-                            course: course,
-                            coursesService: _coursesService,
+          return ListView.builder(
+            itemCount: coursesList.length,
+            itemBuilder: (context, index) {
+              final course = coursesList[index];
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 16.0),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => CoursesDetails(
+                          course: course,
+                          coursesService: _coursesService,
+                        ),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(16.0),
+                    margin: const EdgeInsets.symmetric(
+                        vertical: 4.0, horizontal: 10.0),
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: gradientColors,
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                      ),
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: 150.0,
+                          height: 230.0,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: NetworkImage(course.imageURL),
+                              fit: BoxFit.cover,
+                            ),
+                            borderRadius: BorderRadius.circular(8.0),
                           ),
                         ),
-                      );
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(16.0),
-                      margin: const EdgeInsets.symmetric(
-                          vertical: 4.0, horizontal: 10.0),
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: gradientColors,
-                          begin: Alignment.centerLeft,
-                          end: Alignment.centerRight,
-                        ),
-                        borderRadius: BorderRadius.circular(16.0),
-                      ),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            width: 150.0,
-                            height: 230.0,
-                            decoration: BoxDecoration(
-                              image: DecorationImage(
-                                image: NetworkImage(course.imageURL),
-                                fit: BoxFit.cover,
+                        const SizedBox(width: 16.0),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                course.courseName,
+                                style: TextStyle(
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: textColor,
+                                ),
                               ),
-                              borderRadius: BorderRadius.circular(8.0),
-                            ),
-                          ),
-                          const SizedBox(width: 16.0),
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  course.courseName,
-                                  style: TextStyle(
-                                    fontSize: 20.0,
-                                    fontWeight: FontWeight.bold,
-                                    color: textColor,
-                                  ),
+                              const SizedBox(height: 8.0),
+                              Text(
+                                course.instructor,
+                                style: TextStyle(
+                                  fontSize: 12.0,
+                                  fontWeight: FontWeight.normal,
+                                  color: subtitleColor,
                                 ),
+                              ),
+                              const SizedBox(height: 8.0),
+                              Text(
+                                course.description,
+                                style: TextStyle(
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.normal,
+                                  color: descriptionColor,
+                                ),
+                              ),
+                              const SizedBox(height: 8.0),
+                              Text(
+                                '${course.price} €',
+                                style: TextStyle(
+                                  fontSize: 12.0,
+                                  fontWeight: FontWeight.normal,
+                                  color: priceColor,
+                                ),
+                              ),
+                              if (_userRole == 'admin') ...[
                                 const SizedBox(height: 8.0),
-                                Text(
-                                  course.instructor,
-                                  style: TextStyle(
-                                    fontSize: 12.0,
-                                    fontWeight: FontWeight.normal,
-                                    color: subtitleColor,
-                                  ),
-                                ),
-                                const SizedBox(height: 8.0),
-                                Text(
-                                  course.description,
-                                  style: TextStyle(
-                                    fontSize: 15.0,
-                                    fontWeight: FontWeight.normal,
-                                    color: descriptionColor,
-                                  ),
-                                ),
-                                const SizedBox(height: 8.0),
-                                Text(
-                                  '${course.price} €',
-                                  style: TextStyle(
-                                    fontSize: 12.0,
-                                    fontWeight: FontWeight.normal,
-                                    color: priceColor,
-                                  ),
-                                ),
-                                if (_userRole == 'admin') ...[
-                                  const SizedBox(height: 8.0),
-                                  Align(
-                                    alignment: Alignment.centerRight,
-                                    child: ElevatedButton(
-                                      onPressed: () {
-                                        Navigator.pushReplacement(
-                                          context,
-                                          MaterialPageRoute(
-                                            builder: (context) =>
-                                                UpdateCourseScreen(
-                                              courseId: course.courseId,
-                                            ),
+                                Align(
+                                  alignment: Alignment.centerRight,
+                                  child: ElevatedButton(
+                                    onPressed: () {
+                                      Navigator.pushReplacement(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) =>
+                                              UpdateCourseScreen(
+                                            courseId: course.courseId,
                                           ),
-                                        );
-                                      },
-                                      style: ElevatedButton.styleFrom(
-                                        foregroundColor: Colors.white,
-                                        backgroundColor: Colors.blue,
-                                      ),
-                                      child: const Text('Atualizar'),
+                                        ),
+                                      );
+                                    },
+                                    style: ElevatedButton.styleFrom(
+                                      foregroundColor: Colors.white,
+                                      backgroundColor: Colors.blue,
                                     ),
+                                    child: const Text('Atualizar'),
                                   ),
-                                ],
+                                ),
                               ],
-                            ),
+                            ],
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
-                );
-              },
-            );
-          },
-        ),
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
