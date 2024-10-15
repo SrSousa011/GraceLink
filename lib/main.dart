e5831ccc46a3f6d5cb0bf4bff1c36dba7e6bfbd3
Lucas Sousa <seu@email.com>
2024-10-15 15:44:50 +0200

    [UPDATE] Update navigation flow to include splash screen on app launch

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,8 @@
-import 'package:churchapp/route/root.dart';
-import 'package:churchapp/views/notifications/notification_become_member.dart';
-import 'package:churchapp/views/notifications/notification_event.dart';
-import 'package:churchapp/views/notifications/notification_photo.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'package:churchapp/theme/theme_provider.dart';
 import 'package:churchapp/route/routes.dart';
-import 'package:churchapp/views/nav_bar/nav_bar.dart';
-import 'package:churchapp/views/splash_screen.dart';
-import 'package:churchapp/auth/auth_service.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -17,6 +10,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
   runApp(
     MultiProvider(
       providers: [
@@ -33,22 +27,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context);
-    final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
-
-    NotificationPhotos().init(navigatorKey);
-    NotificationEvents().init(navigatorKey);
-    NotificationBecomeMember().init(navigatorKey);
-
     return MaterialApp(
-      title: 'Grace Link',
-      navigatorKey: navigatorKey,
+      title: 'GraceLink',
       themeMode: themeProvider.themeMode,
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
-      home: SplashScreen(
-        root: Root(auth: AuthenticationService()),
-        drawer: const NavBar(),
-      ),
+      initialRoute: '/',
       onGenerateRoute: AppRoutes.generateRoute,
     );
   }
