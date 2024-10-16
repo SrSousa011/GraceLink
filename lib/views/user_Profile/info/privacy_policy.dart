8e4a3a60a4382aafc7a4c80ecb6e63cf1c08a758
Lucas Sousa <seu@email.com>
2024-10-16 12:29:45 +0200

    [UPDATE] Adjust text size to be smaller

--- lib/views/user_Profile/info/privacy_policy.dart
+++ lib/views/user_Profile/info/privacy_policy.dart
@@ -33,14 +33,14 @@ class PrivacyPolicy extends StatelessWidget {
               'Introdução',
               style: Theme.of(context).textTheme.titleLarge?.copyWith(
                     fontWeight: FontWeight.bold,
-                    fontSize: 18,
+                    fontSize: 16,
                   ),
             ),
             const SizedBox(height: 10),
             Text(
               'Bem-vindo ao aplicativo móvel da nossa igreja. Sua privacidade é importante para nós. Esta Política de Privacidade explica como coletamos, usamos, divulgamos e protegemos suas informações quando você usa nosso aplicativo. Ao usar nosso aplicativo, você concorda com a coleta e uso de informações de acordo com esta política.',
               style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                    fontSize: 16,
+                    fontSize: 15,
                   ),
             ),
             const SizedBox(height: 20),
@@ -48,14 +48,14 @@ class PrivacyPolicy extends StatelessWidget {
               'Coleta de Informações',
               style: Theme.of(context).textTheme.titleLarge?.copyWith(
                     fontWeight: FontWeight.bold,
-                    fontSize: 18,
+                    fontSize: 16,
                   ),
             ),
             const SizedBox(height: 10),
             Text(
               'Podemos coletar informações pessoais que você nos fornece diretamente, como seu nome, endereço de e-mail e outros dados de contato. Também podemos coletar informações sobre o uso do aplicativo, incluindo suas interações e preferências.',
               style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                    fontSize: 16,
+                    fontSize: 14,
                   ),
             ),
             const SizedBox(height: 20),
@@ -63,14 +63,14 @@ class PrivacyPolicy extends StatelessWidget {
               'Uso das Informações',
               style: Theme.of(context).textTheme.titleLarge?.copyWith(
                     fontWeight: FontWeight.bold,
-                    fontSize: 18,
+                    fontSize: 16,
                   ),
             ),
             const SizedBox(height: 10),
             Text(
               'Usamos suas informações para melhorar nossos serviços, comunicar-nos com você e fornecer atualizações e ofertas relevantes. Não vendemos ou alugamos suas informações pessoais a terceiros.',
               style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                    fontSize: 16,
+                    fontSize: 14,
                   ),
             ),
             const SizedBox(height: 20),
@@ -78,14 +78,14 @@ class PrivacyPolicy extends StatelessWidget {
               'Segurança dos Dados',
               style: Theme.of(context).textTheme.titleLarge?.copyWith(
                     fontWeight: FontWeight.bold,
-                    fontSize: 18,
+                    fontSize: 16,
                   ),
             ),
             const SizedBox(height: 10),
             Text(
               'Implementamos medidas de segurança razoáveis para proteger suas informações pessoais contra acesso, uso ou divulgação não autorizados. No entanto, nenhum método de transmissão pela internet ou método de armazenamento eletrônico é 100% seguro.',
               style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                    fontSize: 16,
+                    fontSize: 14,
                   ),
             ),
             const SizedBox(height: 20),
@@ -93,14 +93,14 @@ class PrivacyPolicy extends StatelessWidget {
               'Alterações a Esta Política',
               style: Theme.of(context).textTheme.titleLarge?.copyWith(
                     fontWeight: FontWeight.bold,
-                    fontSize: 18,
+                    fontSize: 16,
                   ),
             ),
             const SizedBox(height: 10),
             Text(
               'Podemos atualizar nossa Política de Privacidade de tempos em tempos. Notificaremos você sobre quaisquer alterações publicando a nova Política de Privacidade nesta página. Recomendamos que você revise esta Política de Privacidade periodicamente para verificar quaisquer mudanças.',
               style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                    fontSize: 16,
+                    fontSize: 14,
                   ),
             ),
             const SizedBox(height: 20),
@@ -108,14 +108,14 @@ class PrivacyPolicy extends StatelessWidget {
               'Entre em Contato Conosco',
               style: Theme.of(context).textTheme.titleLarge?.copyWith(
                     fontWeight: FontWeight.bold,
-                    fontSize: 18,
+                    fontSize: 16,
                   ),
             ),
             const SizedBox(height: 10),
             Text(
               'Se você tiver alguma dúvida sobre esta Política de Privacidade, entre em contato conosco pelo e-mail: info@churchapp.org',
               style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                    fontSize: 16,
+                    fontSize: 14,
                   ),
             ),
           ],
