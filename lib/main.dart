86070b16507bff99564e858503c07fc756d585e5
Lucas Sousa <seu@email.com>
2024-11-19 16:03:23 -0300

    [UPDATE] Migrate to GetMaterialApp and refactor navigation setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:provider/provider.dart';
 import 'package:churchapp/theme/theme_provider.dart';
 import 'package:churchapp/route/routes.dart';
-import 'package:churchapp/views/notifications/notification_become_member.dart';
-import 'package:churchapp/views/notifications/notification_event.dart';
-import 'package:churchapp/views/notifications/notification_photo.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
   final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
-  NotificationPhotos().init(navigatorKey);
-  NotificationEvents().init(navigatorKey);
-  NotificationBecomeMember().init(navigatorKey);
-
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => ThemeProvider()),
       ],
-      child: const MyApp(),
+      child: MyApp(navigatorKey: navigatorKey),
     ),
   );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final GlobalKey<NavigatorState> navigatorKey;
+
+  const MyApp({super.key, required this.navigatorKey});
 
   @override
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context);
 
-    return MaterialApp(
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'GraceLink',
       themeMode: themeProvider.themeMode,
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
       initialRoute: '/',
-      navigatorKey: AppRoutes.navigatorKey,
-      onGenerateRoute: AppRoutes.generateRoute,
+      navigatorKey: navigatorKey,
+      getPages: AppRoutes.getRoutes,
     );
   }
 }
